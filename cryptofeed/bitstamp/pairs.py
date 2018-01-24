f2d1d04b725b4b845d9c678b0eab150a56a6d1ba
Bryant Moscon <bmoscon@gmail.com>
2018-01-23 19:56:45 -0500

    order book for bitstamp

--- cryptofeed/bitstamp/pairs.py
+++ cryptofeed/bitstamp/pairs.py
@@ -5,19 +5,7 @@ Please see the LICENSE file for the terms and conditions
 associated with this software.
 '''
 bitstamp_trading_pairs = {
-    'btcusd',
-    'btceur', 
-    'eurusd', 
-    'xrpusd',
-    'xrpeur', 
-    'xrpbtc', 
-    'ltcusd', 
-    'ltceur',
-    'ltcbtc', 
-    'ethusd', 
-    'etheur', 
-    'ethbtc', 
-    'bchusd', 
-    'bcheur', 
+    'btcusd', 'btceur', 'eurusd', 'xrpusd', 'xrpeur', 'xrpbtc', 'ltcusd',
+    'ltceur', 'ltcbtc', 'ethusd', 'etheur', 'ethbtc', 'bchusd', 'bcheur',
     'bchbtc'
 }
