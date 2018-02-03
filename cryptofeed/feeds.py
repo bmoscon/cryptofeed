7aa28a6f77090946f81cb145829ba2d43ccfec2d
Bryant Moscon <bmoscon@gmail.com>
2018-02-03 12:45:11 -0500

    update

--- cryptofeed/feeds.py
+++ cryptofeed/feeds.py
@@ -5,7 +5,7 @@ Please see the LICENSE file for the terms and conditions
 associated with this software.
 '''
 from cryptofeed.exchanges import BITFINEX, POLONIEX
-from cryptofeed.defines import L2_BOOK, L3_BOOK, TRADES, TICKER
+from cryptofeed.defines import L2_BOOK, L3_BOOK, TRADES, TICKER, UNSUPPORTED
 
 
 
@@ -14,9 +14,9 @@ _exchange_to_feed_map = {
 }
 
 _feed_to_exchange_map = {
-    L2_BOOK: {BITFINEX: 'book-P0-F0-25'},
+    L2_BOOK: {BITFINEX: 'book-P0-F0-25', POLONIEX: UNSUPPORTED},
     L3_BOOK: {BITFINEX: 'book-R0-F0-100'},
-    TRADES:  { },
+    TRADES:  {POLONIEX: UNSUPPORTED},
     TICKER:  {POLONIEX: 1002},
     VOLUME:  {POLONIEX: 1003}
 }
