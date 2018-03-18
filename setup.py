ba3fd575474a4f475ec0c35a1558f053ea559ae5
Bryant Moscon <bmoscon@gmail.com>
2018-03-18 15:53:03 -0400

    update version for releases

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ class Test(TestCommand):
 
 setup(
     name="cryptofeed",
-    version="0.9.3",
+    version="0.10.0",
     author="Bryant Moscon",
     author_email="bmoscon@gmail.com",
     description=("Cryptocurrency feed handler and synthetic NBBO feed"),
