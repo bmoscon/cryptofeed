81e04b83a6350a5322ff3025c0eac36c22b84a5c
Bryant Moscon <bmoscon@gmail.com>
2018-08-06 17:35:28 -0400

    Update Rest class to use bitfinex

--- cryptofeed/rest/rest.py
+++ cryptofeed/rest/rest.py
@@ -1,10 +1,13 @@
 from cryptofeed.rest.bitmex import Bitmex
+from cryptofeed.rest.bitfinex import Bitfinex
+
 
 class Rest:
     def __init__(self, config):
         self.config = config
         self.lookup = {
-            'bitmex': Bitmex(config)
+            'bitmex': Bitmex(config),
+            'bitfinex': Bitfinex(config)
         }
     
     def __getattr__(self, attr):
