83d2d8d48fdd211ab27e4f243802caa957acf7e2
Bryant Moscon <bmoscon@gmail.com>
2018-08-27 19:00:20 -0400

    add missing file

---
+++ cryptofeed/log.py
@@ -0,0 +1,18 @@
+import logging
+
+FORMAT = '%(asctime)-15s : %(levelname)s : %(message)s'
+
+
+def get_logger(name, filename, level=logging.WARNING):
+    logger = logging.getLogger(name)
+    logger.setLevel(level)
+
+    stream = logging.StreamHandler()
+    stream.setFormatter(FORMAT)
+    logger.addHandler(stream)
+
+    fh = logging.FileHandler(filename)
+    fh.setFormatter(FORMAT)
+    logger.addHandler(fh)
+    logger.propagate = False
+    return logger
