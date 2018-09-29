da396bf3ce420abd5c07d0b9f770c804e4f15d6e
Bryant Moscon <bmoscon@gmail.com>
2018-09-29 13:20:54 -0400

    Update test_rest.py

--- tests/integration/rest/test_rest.py
+++ tests/integration/rest/test_rest.py
@@ -2,20 +2,20 @@ from cryptofeed.rest import Rest
 
 
 def test_rest_bitmex():
-    expected = {'timestamp': '2017-01-01T00:00:36.806Z', 
+    expected = {'timestamp': '2018-09-29T00:00:09.939Z',
                 'pair': 'XBTUSD',
-                'id': 'e4db2886-ae1d-4191-c516-566fb703365c',
+                'id': '58db4844-82b2-40e9-de90-c4d1672af7cc',
                 'feed': 'BITMEX',
-                'side': 'Buy',
-                'amount': 500,
-                'price': 968.49}
+                'side': 'Sell',
+                'amount': 265,
+                'price': 6620}
                 
     r = Rest()
     ret = []
-    for data in r.bitmex.trades('XBTUSD', start='2017-01-01 00:00:00', end='2017-01-01 0:00:37'):
+    for data in r.bitmex.trades('XBTUSD', start='2018-09-29 00:00:09.939', end='2018-09-29 00:00:10'):
         ret.extend(data)
     
-    assert len(ret) == 4
+    assert len(ret) == 1
     assert ret[0] == expected
 
 
