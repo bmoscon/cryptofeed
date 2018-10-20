4fab921a6097a32f7b72704091d85714277bef38
Cody Jacques <jacques.co@husky.neu.edu>
2018-10-20 12:35:24 -0400

    finish kraken api.

--- cryptofeed/rest/kraken.py
+++ cryptofeed/rest/kraken.py
@@ -12,10 +12,10 @@ class Kraken(API):
 
     api = "https://api.kraken.com/0"
 
-    def _post_public(self, command: str):
+    def _post_public(self, command: str, payload = {}):
         url = "{}{}".format(self.api, command)
 
-        resp = requests.get(url)
+        resp = requests.post(url, data = payload)
 
         if resp.status_code != 200:
             LOG.error("%s: Status code %d", self.ID, resp.status_code)
@@ -25,10 +25,38 @@ class Kraken(API):
 
         return resp.json()
 
-    def _post_private(self, payload = {}):
+
+    def _post_private(self, command: str, payload = {}):
         # API-Key = API key
         # API-Sign = Message signature using HMAC-SHA512 of (URI path + SHA256(nonce + POST data)) and base64 decoded secret API key
         payload['nonce'] = int(time() * 1000)
+
+        urlpath = '{}{}'.format('/0', command)
+
+        postdata = urllib.parse.urlencode(payload)
+
+        # Unicode-objects must be encoded before hashing
+        encoded = (str(payload['nonce']) + postdata).encode('utf8')
+        message = urlpath.encode() + hashlib.sha256(encoded).digest()
+
+        signature = hmac.new(base64.b64decode(self.key_secret),
+                             message, hashlib.sha512)
+        sigdigest = base64.b64encode(signature.digest())
+
+        headers = {
+            'API-Key': self.key_id,
+            'API-Sign': sigdigest.decode()
+        }
+
+        resp = requests.post("{}{}".format(self.api, command), data = payload, headers = headers)
+
+        if resp.status_code != 200:
+            LOG.error("%s: Status code %d", self.ID, resp.status_code)
+            LOG.error("%s: Headers: %s", self.ID, resp.headers)
+            LOG.error("%s: Resp: %s", self.ID, resp.text)
+            resp.raise_for_status()
+
+        return resp.json()
 
 
     # public API
@@ -93,3 +121,159 @@ class Kraken(API):
             since = return spread data since given id (optional.  inclusive)
         """
         return self._post_public("/public/Spread", payload)
+
+    # Private API
+
+    def get_account_balance(self, payload={}):
+        """
+        Parameters:
+            aclass = asset class (optional)
+            asset = base asset used to determine balance (default = ZUSD
+        """
+        return self._post_private('/private/Balance', payload)
+
+    def get_open_orders(self, payload = {}):
+        """
+        Parameters:
+            trades = whether or not to include trades in output (optional.  default = false)
+            userref = restrict results to given user reference id (optional)
+        """
+        return self._post_private('/private/OpenOrders', payload)
+
+    def get_closed_orders(self, payload = {}):
+        """
+        Parameters:
+            trades = whether or not to include trades in output (optional.  default = false)
+            userref = restrict results to given user reference id (optional)
+            start = starting unix timestamp or order tx id of results (optional.  exclusive)
+            end = ending unix timestamp or order tx id of results (optional.  inclusive)
+            ofs = result offset
+            closetime = which time to use (optional)
+        """
+        return self._post_private('/private/ClosedOrders', payload)
+
+    def query_orders_info(self, payload = {}):
+        """
+        Parameters:
+            txid = comma delimited list of transaction ids to query info about (20 maximum)
+            trades = whether or not to include trades in output (optional.  default = false)
+            userref = restrict results to given user reference id (optional)
+        """
+        return self._post_private('/private/QueryOrders', payload)
+
+    def get_trades_history(self, payload = {}):
+        """
+        Parameters:
+            type = type of trade (optional)
+                all = all types (default)
+                any position = any position (open or closed)
+                closed position = positions that have been closed
+                closing position = any trade closing all or part of a position
+                no position = non-positional trades
+            trades = whether or not to include trades related to position in output (optional.  default = false)
+            start = starting unix timestamp or trade tx id of results (optional.  exclusive)
+            end = ending unix timestamp or trade tx id of results (optional.  inclusive)
+            ofs = result offset
+        """
+        return self._post_private('/private/TradesHistory', payload)
+
+    def query_trades_info(self, payload: dict):
+        """
+        Parameters:
+            txid = comma delimited list of transaction ids to query info about (20 maximum)
+            trades = whether or not to include trades related to position in output (optional.  default = false)
+        """
+        return self._post_private('/private/QueryTrades', payload)
+
+
+    def get_open_positions(self, payload: dict):
+        """
+        Parameters:
+            txid = comma delimited list of transaction ids to restrict output to
+            docalcs = whether or not to include profit/loss calculations (optional.  default = false)
+        """
+        return self._post_private('/private/OpenPositions', payload)
+
+
+    def get_ledgers_info(self, payload = {}):
+        """
+        Parameters:
+            aclass = asset class (optional):
+            currency (default)
+        asset = comma delimited list of assets to restrict output to (optional.  default = all)
+        type = type of ledger to retrieve (optional):
+            all (default)
+            deposit
+            withdrawal
+            trade
+            margin
+        start = starting unix timestamp or ledger id of results (optional.  exclusive)
+        end = ending unix timestamp or ledger id of results (optional.  inclusive)
+        ofs = result offset
+        """
+        return self._post_private('/private/Ledgers', payload)
+
+    def query_ledgers(self, payload: dict):
+        """
+        Parameters:
+            id = comma delimited list of ledger ids to query info about (20 maximum)
+        """
+        return self._post_private('/private/QueryLedgers', payload)
+
+
+    def get_trade_volume(self, payload = {}):
+        """
+        Parameters:
+            pair = comma delimited list of asset pairs to get fee info on (optional)
+            fee-info = whether or not to include fee info in results (optional)
+        """
+        return self._post_private('/private/TradeVolume', payload)
+
+    def add_standard_order(self, payload: dict):
+        """
+        Parameters:
+            pair = asset pair
+            type = type of order (buy/sell)
+            ordertype = order type:
+                market
+                limit (price = limit price)
+                stop-loss (price = stop loss price)
+                take-profit (price = take profit price)
+                stop-loss-profit (price = stop loss price, price2 = take profit price)
+                stop-loss-profit-limit (price = stop loss price, price2 = take profit price)
+                stop-loss-limit (price = stop loss trigger price, price2 = triggered limit price)
+                take-profit-limit (price = take profit trigger price, price2 = triggered limit price)
+                trailing-stop (price = trailing stop offset)
+                trailing-stop-limit (price = trailing stop offset, price2 = triggered limit offset)
+                stop-loss-and-limit (price = stop loss price, price2 = limit price)
+                settle-position
+            price = price (optional.  dependent upon ordertype)
+            price2 = secondary price (optional.  dependent upon ordertype)
+            volume = order volume in lots
+            leverage = amount of leverage desired (optional.  default = none)
+            oflags = comma delimited list of order flags (optional):
+                viqc = volume in quote currency (not available for leveraged orders)
+                fcib = prefer fee in base currency
+                fciq = prefer fee in quote currency
+                nompp = no market price protection
+                post = post only order (available when ordertype = limit)
+            starttm = scheduled start time (optional):
+                0 = now (default)
+                +<n> = schedule start time <n> seconds from now
+                <n> = unix timestamp of start time
+            expiretm = expiration time (optional):
+                0 = no expiration (default)
+                +<n> = expire <n> seconds from now
+                <n> = unix timestamp of expiration time
+            userref = user reference id.  32-bit signed number.  (optional)
+            validate = validate inputs only.  do not submit order (optional)
+        """
+        return self._post_private('/private/AddOrder', payload)
+
+
+    def cancel_open_order(self, payload: dict):
+        """
+        Parameters:
+            txid = transaction id
+        """
+        return self._post_private('/private/CancelOrder', payload)
--- tests/unit/rest_apis/test_kraken.py
+++ tests/unit/rest_apis/test_kraken.py
@@ -44,19 +44,69 @@ class TestKraken(unittest.TestCase):
 
     def test_recent_spread_data(self):
         kraken = Rest('config.yaml').kraken
-        data = kraken.recent_spread_data({"pair": "adacad"})
+        data = kraken.get_recent_spread_data({"pair": "adacad"})
         assert len(data['result']['ADACAD']) > 0
+
+    def test_get_account_balance(self):
+        kraken = Rest('config.yaml').kraken
+        balance = kraken.get_account_balance()
+        assert balance['error'] == []
+
+    def test_get_open_orders(self):
+        kraken = Rest('config.yaml').kraken
+        open_orders = kraken.get_open_orders()
+        assert open_orders['error'] == []
+
+    def test_get_open_orders_trades(self):
+        kraken = Rest('config.yaml').kraken
+        open_orders = kraken.get_open_orders({'trades': 'true'})
+        assert open_orders['error'] == []
+
+    def test_get_closed_orders(self):
+        kraken = Rest('config.yaml').kraken
+        closed_orders = kraken.get_closed_orders()
+        assert closed_orders['error'] == []
+
+    def test_get_closed_orders_trades(self):
+        kraken = Rest('config.yaml').kraken
+        closed_orders = kraken.get_closed_orders({'trades': 'true'})
+        assert closed_orders['error'] == []
+
+    def test_query_orders_info(self):
+        kraken = Rest('config.yaml').kraken
+        orders_info = kraken.query_orders_info()
+        assert orders_info['error'][0] == 'EGeneral:Invalid arguments'
+
+    def test_get_get_trades_history(self):
+        kraken = Rest('config.yaml').kraken
+        trades_history = kraken.get_trades_history()
+        assert trades_history['error'] == []
+
+    def test_get_get_trades_history_params(self):
+        kraken = Rest('config.yaml').kraken
+        trades_history = kraken.get_trades_history({'trades': 'true', 'type': 'any position'})
+        assert trades_history['error'] == []
+
+    def test_get_query_trades_info(self):
+        kraken = Rest('config.yaml').kraken
+        trades_info = kraken.query_trades_info({})
+        assert trades_info['error'][0] == 'EGeneral:Invalid arguments'
+
+
+    def test_get_ledgers_info(self):
+        kraken = Rest('config.yaml').kraken
+        ledgers_info = kraken.get_ledgers_info()
+        assert ledgers_info['error'] == []
+
+
+    def test_get_trade_volume(self):
+        kraken = Rest('config.yaml').kraken
+        trade_volume = kraken.get_trade_volume()
+        assert trade_volume['result']['currency'] == 'ZUSD'
 
 
 if __name__ == '__main__':
     unittest.main()
 
 kraken = Rest('config.yaml').kraken
-kraken._post_public("/public/Assets")
-
-import requests
-resp = requests.post(
-"{}{}".format(kraken.api, "/public/Spread"),
-data = {"pair": "adacad"}
-)
-resp.json()
+kraken.get_account_balance()
