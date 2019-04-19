c51391935a33622efd0ac2cff8e7aa93ddb5d1ac
Bryant Moscon <bmoscon@gmail.com>
2019-04-19 17:23:20 -0400

    Fix unit tests

--- tests/unit/rest_apis/test_poloniex.py
+++ tests/unit/rest_apis/test_poloniex.py
@@ -1,58 +1,29 @@
 import pytest
 
 from cryptofeed.rest import Rest
+from cryptofeed.defines import BID, ASK
 
 
 poloniex = Rest('config.yaml').Poloniex
 
 
 def test_get_ticker():
-    tickers = poloniex.tickers()
-
-    assert 'BTC_BTS' in tickers
-    assert tickers['BTC_BTS']['id'] == 14
+    ticker = poloniex.ticker('BTC-USDT')
+    assert ticker['bid'] > 0
 
 
-def test_past_day_volume():
-    past_day = poloniex.past_day_volume()
+def test_order_book():
+    order_book = poloniex.l2_book('BTC-USDT')
 
-    assert 'BTC_BCN' in past_day
-    assert 'BTC' in past_day['BTC_BCN']
-    assert 'BCN' in past_day['BTC_BCN']
-
-
-def test_order_books():
-    order_books = poloniex.order_books({"currencyPair": "BTC_NXT", "depth": 5})
-
-    assert 'asks' in order_books
-    assert 'bids' in order_books
-    assert len(order_books['asks']) == 5
-
-
-def test_order_books_1():
-    order_books = poloniex.order_books({"currencyPair": "BTC_NXT"})
-
-    assert 'asks' in order_books and 'bids' in order_books
+    assert BID in order_book
+    assert ASK in order_book
+    assert len(order_book[BID]) > 0
 
 
 def test_trade_history():
     trade_history = list(next(poloniex.trades('BTC-USDT', start='2018-10-01', end='2018-10-02')))
     assert len(trade_history) > 0
     assert float(trade_history[0]['amount']) > 0
-
-
-def test_chart_data():
-    chart_data = poloniex.chart_data({"currencyPair": "BTC_XMR", "start": 1405699200, "end": 9999999999, "period": 14400})
-
-    assert len(chart_data) > 0
-    assert chart_data[0]['volume'] > 0
-
-
-def test_currencies():
-    currencies = poloniex.currencies()
-
-    assert '1CR' in currencies
-    assert currencies['1CR']['id'] == 1
 
 
 @pytest.mark.skipif(poloniex.key_id is None or poloniex.key_secret is None, reason="No api key provided")
