723a7704c0496bdc500978e6e592abfe0d57b834
Bryant Moscon <bmoscon@gmail.com>
2019-05-15 17:44:22 -0400

    Tweak bitmex rest endpoint since the historical data via rest periodically expires

--- tests/integration/rest/test_rest.py
+++ tests/integration/rest/test_rest.py
@@ -3,21 +3,12 @@ from cryptofeed.defines import BUY, SELL
 
 
 def test_rest_bitmex():
-    expected = {'timestamp': '2018-09-29T00:00:09.939Z',
-                'pair': 'XBTUSD',
-                'id': '58db4844-82b2-40e9-de90-c4d1672af7cc',
-                'feed': 'BITMEX',
-                'side': SELL,
-                'amount': 265,
-                'price': 6620}
-
     r = Rest()
     ret = []
-    for data in r.bitmex.trades('XBTUSD', start='2018-09-29 00:00:09.939', end='2018-09-29 00:00:10'):
+    for data in r.bitmex.trades('XBTUSD', start='2019-05-01 00:00:09', end='2019-05-01 00:00:15'):
         ret.extend(data)
 
-    assert len(ret) == 1
-    assert ret[0] == expected
+    assert len(ret) == 2
 
 
 def test_rest_bitfinex():
