ec1659b0e25892474b4cf767135727968176127f
Bryant Moscon <bmoscon@gmail.com>
2019-05-17 21:08:39 -0400

    Update NBBO to include size in addition to price

--- cryptofeed/nbbo.py
+++ cryptofeed/nbbo.py
@@ -20,17 +20,21 @@ class NBBO(Callback):
 
         super(NBBO, self).__init__(callback)
 
-    async def _update(self, feed, pair, book, timestamp):
-        self.bids[pair][feed] = Decimal(list(book[BID].keys())[-1])
-        self.asks[pair][feed] = Decimal(list(book[ASK].keys())[0])
+    def _update(self, feed, pair, book, timestamp):
+        bid = Decimal(list(book[BID].keys())[-1])
+        size = book[BID][bid]
+        self.bids[pair][feed] = {'price': bid, 'size': size}
+        ask = Decimal(list(book[ASK].keys())[0])
+        size = book[ASK][ask]
+        self.asks[pair][feed] = {'price': ask, 'size': size}
 
-        min_ask = min(self.asks[pair], key=self.asks[pair].get)
-        max_bid = max(self.bids[pair], key=self.bids[pair].get)
+        min_ask = min(self.asks[pair], key=lambda x: self.asks[pair][x]['price'])
+        max_bid = max(self.bids[pair], key=lambda x: self.bids[pair][x]['price'])
 
         return self.bids[pair][max_bid], self.asks[pair][min_ask], max_bid, min_ask
 
     async def __call__(self, *, feed: str, pair: str, book: dict, timestamp):
-        update = await self._update(feed, pair, book, timestamp)
+        update = self._update(feed, pair, book, timestamp)
 
         # only write updates when a best bid / best aks changes
         if self.last_update == update:
@@ -41,7 +45,7 @@ class NBBO(Callback):
         if bid is None:
             return
         if self.is_async:
-            await self.callback(pair, bid, ask, bid_feed, ask_feed)
+            await self.callback(pair, bid['price'], bid['size'], ask['price'], ask['size'], bid_feed, ask_feed)
         else:
             loop = asyncio.get_event_loop()
-            await loop.run_in_executor(None, self.callback, pair, bid, ask, bid_feed, ask_feed)
+            await loop.run_in_executor(None, self.callback, pair, bid['price'], bid['size'], ask['price'], ask['size'], bid_feed, ask_feed)
--- examples/demo_nbbo.py
+++ examples/demo_nbbo.py
@@ -5,16 +5,16 @@ Please see the LICENSE file for the terms and conditions
 associated with this software.
 '''
 from cryptofeed import FeedHandler
-from cryptofeed.exchanges import Coinbase, Bitfinex, HitBTC, Bitstamp, Gemini
+from cryptofeed.exchanges import Coinbase, Kraken, Gemini
 
 
-def nbbo_update(pair, bid, ask, bid_feed, ask_feed):
-    print('Pair: {} Bid: {:.2f} Bid Feed: {} Ask: {:.2f} Ask Feed: {}'.format(pair, bid, bid_feed, ask, ask_feed))
+def nbbo_update(pair, bid, bid_size, ask, ask_size, bid_feed, ask_feed):
+    print(f'Pair: {pair} Bid Price: {bid:.2f} Bid Size: {bid_size:.6f} Bid Feed: {bid_feed} Ask Price: {ask:.2f} Ask Size: {ask_size:.6f} Ask Feed: {ask_feed}')
 
 
 def main():
     f = FeedHandler()
-    f.add_nbbo([Coinbase, HitBTC, Bitfinex, Bitstamp, Gemini], ['BTC-USD'], nbbo_update)
+    f.add_nbbo([Coinbase, Kraken, Gemini], ['BTC-USD'], nbbo_update)
     f.run()
 
 
