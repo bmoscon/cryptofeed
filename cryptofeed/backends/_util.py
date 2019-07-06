203cd3c9afd8e06ddcc90fe906db499a6c55d62a
Bryant Moscon <bmoscon@gmail.com>
2019-07-05 21:24:13 -0400

    Elastic Cache support

--- cryptofeed/backends/_util.py
+++ cryptofeed/backends/_util.py
@@ -53,3 +53,27 @@ def book_convert(book: dict, data: dict, depth: int, convert=str):
         count += 1
         if depth and count >= depth:
             break
+
+
+def book_flatten(feed: str, pair: str, book: dict, timestamp: float, delta: str) -> dict:
+    """
+    takes book and returns a list of dict, where each element in the list
+    is a dictionary with a single row of book data.
+
+    eg.
+    L2:
+    [{'side': str, 'price': float, 'size': float, 'timestamp': float}, {...}, ...]
+
+    L3:
+    [{'side': str, 'price': float, 'size': float, 'timestamp': float, 'order_id': str}, {...}, ...]
+    """
+    ret = []
+    for side in (BID, ASK):
+        for price, data in book[side].items():
+            if isinstance(data, dict):
+                # L3 book
+                for order_id, size in data.items():
+                    ret.append({'feed': feed, 'pair': pair, 'side': side, 'price': price, 'size': size, 'order_id': order_id, 'timestamp': timestamp, 'delta': delta})
+            else:
+                ret.append({'feed': feed, 'pair': pair, 'side': side, 'price': price, 'size': data, 'timestamp': timestamp, 'delta': delta})
+    return ret
---
+++ cryptofeed/backends/http.py
@@ -0,0 +1,33 @@
+'''
+Copyright (C) 2017-2019  Bryant Moscon - bmoscon@gmail.com
+
+Please see the LICENSE file for the terms and conditions
+associated with this software.
+'''
+import logging
+import aiohttp
+
+
+LOG = logging.getLogger('feedhandler')
+
+
+class HTTPCallback:
+    def __init__(self, addr: str, **kwargs):
+        self.addr = addr
+        self.session = None
+
+    async def write(self, method: str, data, headers=None):
+        if not self.session or self.session.closed:
+            self.session = aiohttp.ClientSession()
+
+        m = None
+        if method.lower() == 'post':
+            m = self.session.post
+        elif method.lower() == 'put':
+            m = self.session.put
+
+        async with m(self.addr, data=data, headers=headers) as resp:
+            if resp.status >= 400:
+                error = await resp.text()
+                LOG.error("%s to %s failed: %d - %s", method.upper(), self.addr, resp.status, error)
+            resp.raise_for_status()
