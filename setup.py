67e142899fe835f68da00ff00d1aa744ee7fd410
Bryant Moscon <bmoscon@gmail.com>
2019-07-06 19:21:04 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ class Test(TestCommand):
 
 setup(
     name="cryptofeed",
-    version="0.25.0",
+    version="1.0.0",
     author="Bryant Moscon",
     author_email="bmoscon@gmail.com",
     description=("Cryptocurrency feed handler and synthetic NBBO feed"),
