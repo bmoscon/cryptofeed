76333613a70679c8ccce7c2db159dd436db1e5aa
glgnohk <>
2019-07-09 15:19:10 +0800

    Added OKEX to standards.py

--- cryptofeed/standards.py
+++ cryptofeed/standards.py
@@ -64,7 +64,7 @@ def pair_exchange_to_std(pair):
 
 
 def timestamp_normalize(exchange, ts):
-    if exchange in {BITMEX, COINBASE, HITBTC, OKCOIN, BYBIT}:
+    if exchange in {BITMEX, COINBASE, HITBTC, OKCOIN, OKEX, BYBIT}:
         return pd.Timestamp(ts).timestamp()
     elif exchange in  {HUOBI, HUOBI_US, BITFINEX, COINBENE, DERIBIT, BINANCE}:
         return ts / 1000.0
