3fd1f232ed0f23c85e4ad485befaa9a10010a9e2
quantfiction <quantfiction1@protonmail.com>
2019-09-16 22:40:53 -0400

    demo file didn't get added last time

---
+++ examples/demo_renko.py
@@ -0,0 +1,30 @@
+'''
+Copyright (C) 2017-2019  Bryant Moscon - bmoscon@gmail.com
+
+Please see the LICENSE file for the terms and conditions
+associated with this software.
+'''
+from cryptofeed.callback import Callback
+from cryptofeed.backends.aggregate import RenkoFixed
+
+from cryptofeed import FeedHandler
+from cryptofeed.exchanges import Bitmex
+from cryptofeed.defines import TRADES
+
+from datetime import datetime
+
+
+async def renko(data=None):
+    print(datetime.utcnow(), data)
+
+
+def main():
+    f = FeedHandler()
+    f.add_feed(Bitmex(pairs=['XBTUSD'], channels=[TRADES], callbacks={
+               TRADES: RenkoFixed(Callback(renko), brick_size=3)}))
+
+    f.run()
+
+
+if __name__ == '__main__':
+    main()
