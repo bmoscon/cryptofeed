8facbfbe4c9dfb023d6e8b5d1905ee5097646d5e
heartcored98 <whwodud9@gmail.com>
2020-01-05 15:09:43 +0000

    Support InfluxDB 2.0 token authentication

--- examples/demo_influxdb.py
+++ examples/demo_influxdb.py
@@ -11,12 +11,36 @@ from cryptofeed.exchanges import Bitmex, Coinbase
 from cryptofeed.defines import TRADES, FUNDING, L2_BOOK, BOOK_DELTA, TICKER
 
 
+
 def main():
+    
     f = FeedHandler()
     f.add_feed(Bitmex(channels=[FUNDING, L2_BOOK],pairs=['XBTUSD'], callbacks={FUNDING: FundingInflux('http://localhost:8086', 'example'), L2_BOOK: BookInflux('http://localhost:8086', 'example', numeric_type=float), BOOK_DELTA: BookDeltaInflux('http://localhost:8086', 'example', numeric_type=float)}))
     f.add_feed(Coinbase(channels=[TRADES], pairs=['BTC-USD'], callbacks={TRADES: TradeInflux('http://localhost:8086', 'example')}))
     f.add_feed(Coinbase(channels=[L2_BOOK], pairs=['BTC-USD'], callbacks={L2_BOOK: BookInflux('http://localhost:8086', 'example', numeric_type=float), BOOK_DELTA: BookDeltaInflux('http://localhost:8086', 'example', numeric_type=float)}))
     f.add_feed(Coinbase(channels=[TICKER], pairs=['BTC-USD'], callbacks={TICKER: TickerInflux('http://localhost:8086', 'example', numeric_type=float)}))
+    
+    """
+    # For InfluxDB 2.0, provide additional org, bucket(instead of db), token and precision(optional) 
+    # [Note] In order to use visualization and aggregation, numeric_type with float is strongly recommended. 
+    
+    ADDR = 'https://localhost:9999'
+    ORG='my-org'
+    BUCKET = 'my-bucket'
+    TOKEN = 'token-something-like-end-with-=='
+    
+    f = FeedHandler()
+    funding_influx = FundingInflux(ADDR, org=ORG, bucket=BUCKET, token=TOKEN, numeric_type=float) 
+    trade_influx = TradeInflux(ADDR, org=ORG, bucket=BUCKET, token=TOKEN, numeric_type=float) 
+    book_influx = BookInflux(ADDR, org=ORG, bucket=BUCKET, token=TOKEN, numeric_type=float) 
+    bookdelta_influx = BookDeltaInflux(ADDR, org=ORG, bucket=BUCKET, token=TOKEN, numeric_type=float) 
+    ticker_influx = TickerInflux(ADDR, org=ORG, bucket=BUCKET, token=TOKEN, numeric_type=float) 
+    
+    f.add_feed(Bitmex(channels=[FUNDING, L2_BOOK],pairs=['XBTUSD'], callbacks={FUNDING: funding_influx, L2_BOOK: book_influx, BOOK_DELTA: bookdelta_influx}))
+    f.add_feed(Coinbase(channels=[TRADES], pairs=['BTC-USD'], callbacks={TRADES: trade_influx}))
+    f.add_feed(Coinbase(channels=[L2_BOOK], pairs=['BTC-USD'], callbacks={L2_BOOK: book_influx, BOOK_DELTA: bookdelta_influx}))
+    f.add_feed(Coinbase(channels=[TICKER], pairs=['BTC-USD'], callbacks={TICKER: ticker_influx}))
+    """
 
     f.run()
 
