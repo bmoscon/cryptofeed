608d894173ad3b3f99b70ddd86268d835a3463c2
Bryant Moscon <bmoscon@gmail.com>
2020-05-22 19:43:25 -0400

    bump version

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ class Test(TestCommand):
 
 setup(
     name="cryptofeed",
-    version="1.4.0",
+    version="1.4.1",
     author="Bryant Moscon",
     author_email="bmoscon@gmail.com",
     description=("Cryptocurrency feed handler and synthetic NBBO feed"),
