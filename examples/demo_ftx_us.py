da7bf73ce319fd4f596e4599c154c9790c194018
Adam <30326744+AlgoTrader5@users.noreply.github.com>
2020-06-09 18:23:17 -0500

    Update demo_ftx_us.py
    added l2_book

--- examples/demo_ftx_us.py
+++ examples/demo_ftx_us.py
@@ -6,10 +6,10 @@ associated with this software.
 '''
 from decimal import Decimal
 
-from cryptofeed.callback import TickerCallback, TradeCallback
+from cryptofeed.callback import TickerCallback, TradeCallback, BookCallback
 from cryptofeed import FeedHandler
 from cryptofeed.exchanges import FTXUS
-from cryptofeed.defines import TRADES, TICKER
+from cryptofeed.defines import L2_BOOK, BID, ASK, TRADES, TICKER
 
 
 # Examples of some handlers for different updates. These currently don't do much.
@@ -28,11 +28,13 @@ async def trade(feed, pair, order_id, timestamp, side, amount, price, receipt_ti
     assert isinstance(price, Decimal)
     print(f"Timestamp: {timestamp} Cryptofeed Receipt: {receipt_timestamp} Feed: {feed} Pair: {pair} ID: {order_id} Side: {side} Amount: {amount} Price: {price}")
 
+async def book(feed, pair, book, timestamp, receipt_timestamp):
+    print(f'Timestamp: {timestamp} Feed: {feed} Pair: {pair} Book Bid Size is {len(book[BID])} Ask Size is {len(book[ASK])}')
 
-
+    
 def main():
     f = FeedHandler()
-    f.add_feed(FTXUS(pairs=['BTC-USD', 'BCH-USD', 'USDT-USD'], channels=[TICKER], callbacks={TICKER: ticker, TRADES: TradeCallback(trade)}))
+    f.add_feed(FTXUS(pairs=['BTC-USD', 'BCH-USD', 'USDT-USD'], channels=[TRADES, L2_BOOK, TICKER], callbacks={L2_BOOK: BookCallback(book), TICKER: ticker, TRADES: TradeCallback(trade)}))
     f.run()
 
 
