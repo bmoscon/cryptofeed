0cc07de697267addc519fd32005f323cbd4732ca
O. Libre <olibre@Lmap.org>
2020-07-27 16:01:49 +0200

    Reformat Python source code in setup.py

--- setup.py
+++ setup.py
@@ -10,22 +10,24 @@ from setuptools import setup
 from setuptools import find_packages
 from setuptools.command.test import test as TestCommand
 
+
 def get_long_description():
     """Read the contents of README.md, INSTALL.md and CHANGES.md files."""
     from os import path
+
     repo_dir = path.abspath(path.dirname(__file__))
-    markdown=[]
-    for filename in ['README.md', 'INSTALL.md', 'CHANGES.md']:
-        with open(path.join(repo_dir, filename), encoding='utf-8') as markdown_file:
+    markdown = []
+    for filename in ["README.md", "INSTALL.md", "CHANGES.md"]:
+        with open(path.join(repo_dir, filename), encoding="utf-8") as markdown_file:
             markdown.append(markdown_file.read())
     return "\n\n----\n\n".join(markdown)
+
 
 class Test(TestCommand):
     def run_tests(self):
         import pytest
         errno = pytest.main(['tests/'])
         sys.exit(errno)
-
 
 
 setup(
@@ -62,20 +64,30 @@ setup(
         "pandas",
         "aiohttp==3.6.2",
         "aiofile",
-        'yapic.json>=1.4.3',
+        "yapic.json>=1.4.3",
         # Two (optional) dependencies that speed up Cryptofeed:
-        'aiodns>=1.1',   # aiodns speeds up DNS resolving
-        'cchardet',      # cchardet is a faster replacement for chardet
-],
+        "aiodns>=1.1",  # aiodns speeds up DNS resolving
+        "cchardet",     # cchardet is a faster replacement for chardet
+    ],
     extras_require={
-        'rest_api': ['pyyaml'],
-        'redis': ['aioredis'],
-        'arctic': ['arctic'],
-        'zmq': ['pyzmq'],
-        'mongo': ['motor'],
-        'kafka': ['aiokafka'],
-        'rabbit': ['aio_pika', 'pika'],
-        'postgres': ['asyncpg'],
-        'all': ['pyyaml', 'aioredis', 'arctic', 'pyzmq', 'motor', 'aiokafka', 'aio_pika', 'pika', 'asyncpg'],
+        "rest_api": ["pyyaml"],
+        "redis": ["aioredis"],
+        "arctic": ["arctic"],
+        "zmq": ["pyzmq"],
+        "mongo": ["motor"],
+        "kafka": ["aiokafka"],
+        "rabbit": ["aio_pika", "pika"],
+        "postgres": ["asyncpg"],
+        "all": [
+            "pyyaml",
+            "aioredis",
+            "arctic",
+            "pyzmq",
+            "motor",
+            "aiokafka",
+            "aio_pika",
+            "pika",
+            "asyncpg",
+        ],
     },
 )
