0f6121afc35b670e902e0894acfd9ba6b69c9ff2
rhotchkiss <trebor.hotchkiss@gmail.com>
2020-08-05 22:02:27 +0100

    Update demo_okex_futures.py
    Add callbacks for TICKER_FUTURES, TRADES_FUTURES and L2_BOOK_FUTURES without which the callback in feedhandler will throw KeyErrors when these appear. NB that I am not seeing L2_BOOK_FUTURES appear but have added for completeness.

--- examples/demo_okex_futures.py
+++ examples/demo_okex_futures.py
@@ -1,6 +1,5 @@
 '''
 Copyright (C) 2017-2020  Bryant Moscon - bmoscon@gmail.com
-
 Please see the LICENSE file for the terms and conditions
 associated with this software.
 '''
@@ -11,11 +10,13 @@ from cryptofeed.exchanges import OKEx
 
 
 async def trade(feed, pair, order_id, timestamp, side, amount, price, receipt_timestamp):
-    print(f"Timestamp: {timestamp} Feed: {feed} Pair: {pair} ID: {order_id} Side: {side} Amount: {amount} Price: {price}")
+    print(
+        f"Timestamp: {timestamp} Feed: {feed} Pair: {pair} ID: {order_id} Side: {side} Amount: {amount} Price: {price}")
 
 
 async def book(feed, pair, book, timestamp, receipt_timestamp):
-    print(f'Timestamp: {timestamp} Feed: {feed} Pair: {pair} Book Bid Size is {len(book[BID])} Ask Size is {len(book[ASK])}')
+    print(f'Timestamp: {timestamp} Feed: {feed} Pair: {pair} Book Bid Size is {len(book[BID])} Ask Size is '
+          f'{len(book[ASK])}')
 
 
 async def ticker(feed, pair, bid, ask, timestamp, receipt_timestamp):
@@ -25,7 +26,9 @@ async def ticker(feed, pair, bid, ask, timestamp, receipt_timestamp):
 def main():
     fh = FeedHandler()
 
-    callbacks = {TRADES: TradeCallback(trade), L2_BOOK: BookCallback(book), TICKER: TickerCallback(ticker)}
+    callbacks = {TRADES: TradeCallback(trade), L2_BOOK: BookCallback(book), TICKER: TickerCallback(ticker),
+                 TICKER_FUTURES: TickerCallback(ticker), TRADES_FUTURES: TradeCallback(trade),
+                 L2_BOOK_FUTURES: BookCallback(book)}
     pairs = OKEx.get_active_symbols()
     fh.add_feed(OKEx(pairs=pairs, channels=[TRADES_FUTURES, L2_BOOK_FUTURES, TICKER_FUTURES], callbacks=callbacks))
 
