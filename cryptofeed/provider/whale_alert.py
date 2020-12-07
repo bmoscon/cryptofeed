7fff16f8aaa5ecd99dbd59184a9974aff14ec9e3
Pierre JUILLARD <pierre.juillard@gmail.com>
2020-12-07 21:25:31 +0100

    Simplification of API query for WHALE_ALERT.

--- cryptofeed/provider/whale_alert.py
+++ cryptofeed/provider/whale_alert.py
@@ -7,9 +7,6 @@ associated with this software.
 import asyncio
 import aiohttp
 import logging
-from sortedcontainers import SortedDict
-from functools import reduce
-from operator import iconcat
 from time import time
 import json
 from json import JSONDecodeError
@@ -42,7 +39,7 @@ class WhaleAlert(RestFeed):
 
             kwargs['max_history'] (string - optional):
                 Maximal allowed history, depending on your pricing plan. Format is of type '1H' (for 1 hour) or '1D' (for 1 day).
-                Maximal history can only be specific with hours or days. By default, is 1 hour, as per free plan.
+                Maximal history can only be specified with hours or days. By default, is 1 hour, as per free plan.
 
         """
         self.sleep_time = kwargs.pop('sleep_time') if 'sleep_time' in kwargs else 6                      # Free plan is one request every 6 seconds.
@@ -69,17 +66,13 @@ class WhaleAlert(RestFeed):
         else:
             self.max_history = max_history
 
-        # Following variables are voluntarily not reseted in `__reset()` to avoid storing twice the same data after a reset.
-        # SortedDict storing per last cleared timestamp corresponding coins.
-        # SortedDict({last_cleared_timestamp: [coin1, coin2, ...], ...})
-        self.last_transaction_update = SortedDict()
-        # Whale Alert uses second precise timestamps (not millisecond) which is why making chained calls is justified.
+        # `self.last_transaction_update` is voluntarily not reseted in `__reset()` to avoid storing twice the same data after a reset.
+        # dict({coin: (latest_cleared_timestamp, cursor, query_start_timestamp),...})
+        # Whale Alert uses second-precise timestamps (not millisecond) which is why making chained calls is justified.
         # Making chained calls prevent having holes in the list of transactions.
-        # To make a chained call, it is necessary to use same timestamp as that of the 1st call, and the cursor given in the previous call.
-        # dict({coin: (cursor, first_call_timestamp),...})
-        self.chained_call = dict()
-        # Dict to store transaction data till they are popped out at callback call.
-        self.buffer_transactions = dict()
+        # To make a chained call, it is necessary to use same timestamp as that of the 1st call (`query_start_timestamp`),
+        # and `cursor` given in previous response.
+        self.last_transaction_update = dict()
 
 
     async def subscribe(self):
@@ -109,75 +102,48 @@ class WhaleAlert(RestFeed):
     async def _transactions(self, session, coin):
         """
         Data from /transactions?api_key=_&min_value=_&start=_&currency=_
-        Query strategy seeks:
-            - 1/ to maximize number of data retrieved despite limited number of possible queries.
-            - 2/ to prevent any missing data between 2 queries, even in case of simultaneous transactions,
-              despite the limit of 100 results per query.
-        To do so:
-            - for 1/ coins are prioritized in the query order from the one with the oldest transaction, to the coin with the newest transaction.
-            - for 2/ implementation makes use of chained calls (use of cursor)
+        Query strategy seeks to prevent any missing data between 2 queries, even in case of simultaneous transactions,
+        by use of 'cursor' when appropriate ('chained calls').
         """
 
         receipt_timestamp = int(time())
         last_trans_up = self.last_transaction_update
-        # Step 1 / identification of coin and start timestamp for query to be issued.
-        # If no query has been done so far for requested coin, it is kept.
-        # Otherwise, coin with the oldest transaction time is queried first.
-        max_history_ts = receipt_timestamp-self.max_history+2  # Using 2s margin for the algo to issue the query and still being within `self.max_history`.
+        # Using 2s margin for the algo to issue the query and still being within `self.max_history`.
+        max_history_ts = receipt_timestamp - self.max_history + 2
 
-        if coin in reduce(iconcat, last_trans_up.values(), []):
-            latest_cleared_ts, query_coin_l = last_trans_up.popitem(index=0)
-            query_coin = query_coin_l.pop(0)
-            if query_coin_l != []:
-                last_trans_up[latest_cleared_ts] = query_coin_l
-            # `query_start_ts` is overwritten in case a chained call is to be made.
-            query_cursor, query_start_ts = self.chained_call.pop(query_coin, ('', latest_cleared_ts))
-            if not query_cursor and latest_cleared_ts < max_history_ts:
-                LOG.warning("{!s} - Possible hole in transaction data for coin {!s} due to impossibility to query far enough, back in time.".format(self.id, query_coin))
-                query_start_ts = max_history_ts
-        else:
-            query_coin = coin
+        # Initialize `cursor` and `query_start_ts`.
+        try:
+            latest_cleared_ts, cursor, query_start_ts = last_trans_up[coin]
+            if not cursor:
+                if latest_cleared_ts < max_history_ts:
+                    LOG.warning("{!s} - Possible hole in transaction data for coin {!s} due to impossibility to query far enough, back in time.".format(self.id, coin))
+                    query_start_ts = max_history_ts
+                else:
+                    query_start_ts = latest_cleared_ts
+        except:
+            cursor = ''
             query_start_ts = max_history_ts
-            query_cursor = ''
-            latest_cleared_ts = 0
+            latest_cleared_ts = max_history_ts
 
-        # Step 2 / API query.
-        query = f"{self.address}transactions?api_key={self.key_id}&min_value={self.trans_min_value}&start={query_start_ts}&currency={query_coin}&cursor={query_cursor}" \
-                if query_cursor else f"{self.address}transactions?api_key={self.key_id}&min_value={self.trans_min_value}&start={query_start_ts}&currency={query_coin}"
+
+        # Query.
+        query = f"{self.address}transactions?api_key={self.key_id}&min_value={self.trans_min_value}&start={query_start_ts}&currency={coin}&cursor={cursor}" \
+                if cursor else f"{self.address}transactions?api_key={self.key_id}&min_value={self.trans_min_value}&start={query_start_ts}&currency={coin}"
 
         async with session.get(query) as response:
             data = await response.read()
             try:
                 data = json.loads(data)
             except JSONDecodeError as jde:
-                # Re-insert entries in self.last_transaction_update & self.chained_call before exiting.
-                if latest_cleared_ts:
-                    if latest_cleared_ts in last_trans_up:
-                        last_trans_up[latest_cleared_ts].append(query_coin)
-                    else:
-                        last_trans_up[latest_cleared_ts] = [query_coin]
-                if query_cursor:
-                    self.chained_call[query_coin] = (query_cursor, query_start_ts)
-
                 raise Exception('Returned error: {!s}\nReturned response content from HTTP request: {!s}'.format(jde, data))
-            
-            if data['result'] == 'error':
-                # Re-insert entries in self.last_transaction_update & self.chained_call before exiting.
-                if latest_cleared_ts:
-                    if latest_cleared_ts in last_trans_up:
-                        last_trans_up[latest_cleared_ts].append(query_coin)
-                    else:
-                        last_trans_up[latest_cleared_ts] = [query_coin]
-                if query_cursor:
-                    self.chained_call[query_coin] = (query_cursor, query_start_ts)
 
+            if data['result'] == 'error':
                 raise RestResponseError('Error message in response: {!s}'.format(json_data['message']))
 
-            latest_cleared_ts = receipt_timestamp-1200  # Using 20mn margin for Whale Alert to insert a new entry in their database.
+            # Keeping previous `latest_cleared_ts` in `max_trans_ts` in case there is no new transactions.
+            max_trans_ts = latest_cleared_ts
+            latest_cleared_ts = receipt_timestamp-1200  # Leaving 20mn margin for Whale Alert to insert a new entry in their database.
             if 'transactions' in data:
-                if query_coin not in self.buffer_transactions:
-                    self.buffer_transactions[query_coin] = []
-                max_trans_ts = 0
                 for transaction in data['transactions']:
                     # Flattening the nested dicts.
                     # 'Owner' is not provided if not known. Forcing it as '' into the dict so that DataFrame remains consistent in Cryptostore.
@@ -186,42 +152,31 @@ class WhaleAlert(RestFeed):
                     fro = transaction.pop('from')
                     fro = {('from_' + k): (fro[k] if k in fro else '') for k in to_from_data}
                     del transaction['symbol']  # removing duplicate data with `pair` that is added
-                    # Store in buffer
-                    self.buffer_transactions[query_coin].append({**transaction, **to, **fro})
+                    await self.callback(TRANSACTIONS,
+                                        feed=self.id,
+                                        pair=pair_exchange_to_std(coin),
+                                        # `timestamp` is already with the correct format in `transaction` dict (in unit second).
+                                        **transaction, **to, **fro)
                     max_trans_ts = transaction['timestamp'] if transaction['timestamp'] > max_trans_ts else max_trans_ts
 
-                ## Comments regarding `latest_cleared_ts`:
-                # From doc. : "Some transactions might be reported with a small delay."
-                # From mail exchange with support: "That line is there as a disclaimer in case anything goes wrong.
-                # In general (99.99% of the time) transactions are added instantly."
-                # Hence the 20mn substracted from `receipt_time` before the `for` loop.
-                # Conditions to make a chained call next time (to make sure not to miss any transactions):
-                #  - latest transaction is no older than 20mn,
-                #  - data['count'] is 100.
-                # Otherwise `latest_cleared_ts` will be used for next call (not a chained call).
-                ## Comments regarding `data['count']`:
-                # Number of results per query is limited to 100.
-                # If we have 100 results in the query, we are not certain the last result is the last transaction up to the receipt time or not.
-                # If it is lower than 100, we know there is no more transactions till the receipt time.
-                # This `latest_cleared_ts` will not be used as start ts for the next query (use of chained call), but only for knowing when to do the next query.
-                if max_trans_ts > latest_cleared_ts or data['count'] == 100:
-                    self.chained_call[query_coin] = (data['cursor'], query_start_ts)
-                    latest_cleared_ts = max_trans_ts
-
-            # If there has not been any transactions, latest cleared timestamp is receipt timestamp - 20mn margin.
-            if latest_cleared_ts in last_trans_up:
-                last_trans_up[latest_cleared_ts].append(query_coin)
+            ## Comments regarding `latest_cleared_ts`:
+            # From doc. : "Some transactions might be reported with a small delay."
+            # From mail exchange with support: "That line is there as a disclaimer in case anything goes wrong.
+            # In general (99.99% of the time) transactions are added instantly."
+            # From experience, 40s delay is not uncommon.
+            # Hence the 20mn substracted from `receipt_time` before the `for` loop.
+            # Conditions to make a chained call next time (to make sure not to miss any transactions):
+            #  - latest transaction is no older than 20mn,
+            #  - data['count'] is 100.
+            # Otherwise `latest_cleared_ts` will be used for next call (not a chained call).
+            ## Comments regarding `data['count']`:
+            # Number of results per query is limited to 100.
+            # If we have 100 results in the query, we are not certain the last result is the last transaction up to the receipt time or not.
+            # If it is lower than 100, we know there is no more transactions till the receipt time.
+            # This `latest_cleared_ts` will not be used as start ts for the next query (use of chained call).
+            if max_trans_ts > latest_cleared_ts or data['count'] == 100:
+                last_trans_up[coin] = (max_trans_ts, data['cursor'], query_start_ts)
             else:
-                last_trans_up[latest_cleared_ts] = [query_coin]
-
-        # Step 3 / feed the callback with transactions for `coin` initially requested.
-        if coin in self.buffer_transactions:
-            # Flush data for `coin` in `self.buffer_transactions`.
-            for trans in self.buffer_transactions.pop(coin):
-                await self.callback(TRANSACTIONS,
-                                    feed=self.id,
-                                    pair=pair_exchange_to_std(coin),
-                                    # `timestamp` is already with the correct format in `transaction` dict (in unit second).
-                                    **trans)
+                last_trans_up[coin] = (latest_cleared_ts, '', '')
 
         return
