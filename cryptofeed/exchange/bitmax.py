8cfd98c4fb4e28fff0569daf03a41b29d419e4be
O. Libre <olibre@Lmap.org>
2021-01-17 04:48:29 +0100

    Limit websocket to 10 subscriptions in _address()

--- cryptofeed/exchange/bitmax.py
+++ cryptofeed/exchange/bitmax.py
@@ -4,18 +4,21 @@ Copyright (C) 2017-2021  Bryant Moscon - bmoscon@gmail.com
 Please see the LICENSE file for the terms and conditions
 associated with this software.
 '''
+import random
 from collections import defaultdict
 import logging
 from decimal import Decimal
+from typing import Dict, List, Tuple
 
 from sortedcontainers import SortedDict as sd
 from yapic import json
 
+from cryptofeed.connection import AsyncConnection, WSAsyncConn
 from cryptofeed.defines import BID, ASK, BITMAX, BUY, L2_BOOK, SELL, TRADES
 from cryptofeed.exceptions import MissingSequenceNumber
 from cryptofeed.feed import Feed
 from cryptofeed.standards import symbol_exchange_to_std, timestamp_normalize
-
+from cryptofeed.util import split
 
 LOG = logging.getLogger('feedhandler')
 
@@ -26,10 +29,32 @@ class Bitmax(Feed):
     def __init__(self, **kwargs):
         super().__init__('wss://bitmax.io/0/api/pro/v1/stream', **kwargs)
         self.__reset()
+        self.address = self._address('wss://bitmax.io/0/api/pro/v1/stream')
 
     def __reset(self):
+        # TODO: store context data in conn.ctx
         self.l2_book = {}
         self.seq_no = defaultdict(lambda: None)
+
+    def _address(self, ws_endpoint: str) -> Dict[Tuple[Tuple[str, str]], str]:
+        """
+        When 11 pair-channels in one subscribe msg, BITMAX responds
+        "Invalid operation: No more than 10 symbols per subscribe operation.".
+        When sending multiple "sub" messages in the same websocket, BITMAX responds the same error.
+        """
+        pair_channels: List[Tuple[str, str]] = []
+        for chan in set(self.channels or self.subscription):
+            for pair in set(self.symbols or self.subscription[chan]):
+                pair_channels.append((pair, chan))
+
+        # mix pair/channel combinations to avoid having most of the BTC & USD pairs in the same websocket
+        random.shuffle(pair_channels)
+
+        address: Dict[Tuple[Tuple[str, str]], str] = {}
+        for chunk in split.list_by_max_items(pair_channels, 10):
+            address[tuple(chunk)] = ws_endpoint
+        LOG.info("%s: prepared %s WS subscriptions using %s", self.id, len(address), ws_endpoint)
+        return address
 
     async def _trade(self, msg: dict, timestamp: float):
         """
@@ -88,9 +113,13 @@ class Bitmax(Feed):
 
         await self.book_callback(self.l2_book[pair], L2_BOOK, pair, forced, delta, timestamp_normalize(self.id, msg['data']['ts']), timestamp)
 
-    async def message_handler(self, msg: str, conn, timestamp: float):
+    async def handle(self, data: bytes, timestamp: float, conn: AsyncConnection):
+        """
+        {'m': 'depth', 'ts': 0, 'seqnum': 0, 's': 'ETH/PAX', 'asks': [], 'bids': []}
+        """
+        assert isinstance(conn, WSAsyncConn)
 
-        msg = json.loads(msg, parse_float=Decimal)
+        msg = json.loads(data, parse_float=Decimal)
 
         if 'm' in msg:
             if msg['m'] == 'depth' or msg['m'] == 'depth-snapshot':
@@ -104,24 +133,33 @@ class Bitmax(Feed):
             elif msg['m'] == 'sub':
                 return
             else:
-                LOG.warning("%s: Invalid message type %s", self.id, msg)
+                LOG.warning("%s: Invalid message type %s", conn.id, msg)
         else:
-            LOG.warning("%s: Invalid message type %s", self.id, msg)
+            LOG.warning("%s: Invalid message type %s", conn.id, msg)
 
-    async def subscribe(self, websocket):
+    async def subscribe(self, conn: AsyncConnection):
+        assert isinstance(conn, WSAsyncConn)
+        assert 'opt' in conn.ctx
+        assert isinstance(conn.ctx['opt'], Tuple)
+        opt: Tuple[Tuple[str, str]] = conn.ctx['opt']
         self.__reset()
-        l2_pairs = []
 
-        for channel in self.channels if not self.subscription else self.subscription:
-            pairs = self.symbols if not self.subscription else self.subscription[channel]
+        assert opt
+        for symbol, chan in opt:
+            if chan == "depth:":
+                message = {"op": "req", "action": "depth-snapshot", "args": {"symbol": symbol}}
+            else:
+                message = {'op': 'sub', 'ch': chan + symbol}
+            await conn.send(json.dumps(message))
 
-            if channel == "depth:":
-                l2_pairs.extend(pairs)
+        # # Fallback if options is None
+        # for chan in set(self.channels or self.subscription):
+        #     symbols = set(self.symbols or self.subscription[chan])
+        #     for chunk in split.list_by_max_items(symbols, 10):
+        #         message = {'op': 'sub', 'ch': chan + ','.join(chunk)}
+        #         await conn.send(json.dumps(message))
+        #     if chan == "depth:":
+        #         for symbol in symbols:
+        #             message = {"op": "req", "action": "depth-snapshot", "args": {"symbol": symbol}}
+        #             await conn.send(json.dumps(message))
 
-            pairs = self.symbols if not self.subscription else self.subscription[channel]
-            message = {'op': 'sub', 'ch': channel + ','.join(pairs)}
-            await websocket.send(json.dumps(message))
-
-        for pair in l2_pairs:
-            message = {"op": "req", "action": "depth-snapshot", "args": {"symbol": pair}}
-            await websocket.send(json.dumps(message))
