0b1a2688ef1db6d81c9cd227d227448896f5fbf8
Michael Zhao <mr_michaelzhao@hotmail.com>
2021-04-01 18:30:31 +0100

    Add Deribit callbacks for zmq

--- examples/demo_zmq.py
+++ examples/demo_zmq.py
@@ -5,13 +5,19 @@ Please see the LICENSE file for the terms and conditions
 associated with this software.
 '''
 from multiprocessing import Process
+import asyncio
+from decimal import Decimal
+import datetime
+from collections import defaultdict
+import functools
 
 from yapic import json
 
 from cryptofeed import FeedHandler
-from cryptofeed.backends.zmq import BookZMQ, TickerZMQ
-from cryptofeed.defines import L2_BOOK, TICKER
-from cryptofeed.exchanges import Coinbase, Kraken
+from cryptofeed.backends.zmq import DeribitBookZMQ, DeribitTickerZMQ, DeribitTradeZMQ
+from cryptofeed.defines import BID, ASK, TRADES, L2_BOOK, PERPETURAL, OPTION, FUTURE, ANY, TICKER, USER_TRADES
+from cryptofeed.exchanges import Deribit
+from cryptofeed.util.instrument import get_instrument_type
 
 
 def receiver(port):
@@ -29,6 +35,63 @@ def receiver(port):
         print(key)
         print(json.loads(msg))
 
+async def do_periodically_at(hour, minute, second, periodic_function):
+    while True:
+        t = datetime.datetime.today()
+        future = datetime.datetime(t.year, t.month, t.day, hour, minute, second)
+        if t >= future:
+            print(f'{t} > {future}')
+            future += datetime.timedelta(days=1)
+        delay = (future - t).total_seconds()
+        h = (int)(delay/3600)
+        m = int((delay-h*3600)/60)
+        s = (int)(delay%60)
+        print(f'Waiting {h} hours {m} minutes {s} seconds to execute periodic function')
+        await asyncio.sleep(delay)
+        await periodic_function()
+
+def get_new_subscription():
+    symbols = Deribit.get_instruments()
+    new_subscription = defaultdict(set)
+    for symbol in symbols:
+        for feed_type in subscription[get_instrument_type(symbol)]:
+            new_subscription[feed_type].add(symbol)
+    return new_subscription
+
+async def subscribe_to_new_subscription(deribit):
+    await deribit.update_subscription(subscription=get_new_subscription())
+    # wait a little to let the clock tick on
+    await asyncio.sleep(5)
+
+def get_time_from_timestamp(timestamp):
+    s, ms = divmod(timestamp, 1000)
+    return '%s.%03d' % (datetime.datetime.utcfromtimestamp(s).strftime('%H:%M:%S'), ms)
+
+subscription = {
+    PERPETURAL: [],
+    OPTION: [],
+    FUTURE: []
+
+    # PERPETURAL: [TICKER, TRADES],
+    # OPTION: [TICKER, L2_BOOK],
+    # FUTURE: [TICKER]
+
+    # PERPETURAL: [TICKER, TRADES],
+    # OPTION: [TICKER, TRADES, L2_BOOK],
+    # FUTURE: [TICKER]
+}
+
+other_subscription = {
+    TRADES: [FUTURE, OPTION],
+    USER_TRADES: [ANY],
+}
+
+callbacks = {
+    L2_BOOK: DeribitBookZMQ(port=5678), 
+    TRADES: DeribitTradeZMQ(port=5678), 
+    TICKER: DeribitTickerZMQ(port=5678),
+    # USER_TRADES: trade,
+}
 
 def main():
     try:
@@ -36,11 +99,16 @@ def main():
 
         p.start()
 
-        f = FeedHandler()
-        f.add_feed(Kraken(max_depth=1, channels=[L2_BOOK], symbols=['ETH-USD'], callbacks={L2_BOOK: BookZMQ(port=5678)}))
-        f.add_feed(Coinbase(channels=[TICKER], symbols=['BTC-USD'], callbacks={TICKER: TickerZMQ(port=5678)}))
-
-        f.run()
+        f = FeedHandler(config="config.yaml")
+        all_subscription = get_new_subscription()
+        for key, value in other_subscription.items():
+            all_subscription[key].update(value)
+        print(all_subscription)
+        deribit = Deribit(config="config.yaml", max_depth=1, subscription=all_subscription, callbacks=callbacks)
+        f.add_feed(deribit)
+        # f.add_feed(Deribit(max_depth=1, channels=[L2_BOOK], symbols=['BTC-PERPETUAL'], callbacks={L2_BOOK: BookZMQ(port=5678)}))
+        # f.add_feed(Coinbase(channels=[TICKER], symbols=['BTC-USD'], callbacks={TICKER: TickerZMQ(port=5678)}))
+        f.run(start_loop=True, tasks=[do_periodically_at(8, 1, 1, functools.partial(subscribe_to_new_subscription, deribit))])
 
     finally:
         p.terminate()
