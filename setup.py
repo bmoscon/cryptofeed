5e25018a86bf0b5b87e5a742e111c5f888f2e418
Bryant Moscon <bmoscon@gmail.com>
2021-04-01 20:37:49 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ class Test(TestCommand):
 
 setup(
     name="cryptofeed",
-    version="1.8.1",
+    version="1.8.2",
     author="Bryant Moscon",
     author_email="bmoscon@gmail.com",
     description="Cryptocurrency Exchange Websocket Data Feed Handler",
