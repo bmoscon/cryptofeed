2f0054483df29ddf49c43056641191f20a4c6838
twmeggs <twmeggs@gmail.com>
2021-07-05 12:35:43 +0000

    update get server time for new endpoint

--- cryptofeed/auth/okex.py
+++ cryptofeed/auth/okex.py
@@ -7,23 +7,19 @@ associated with this software.
 import base64
 import hmac
 import requests
-from dateutil.parser import parse
-
 
 def get_server_time():
-    url = "https://www.okex.com/api/general/v3/time"
+    url = "https://www.okex.com/api/v5/public/time"
     response = requests.get(url)
     if response.status_code == 200:
-        return response.json()['iso']
+        return response.json()['data'][0]['ts']
     else:
         return ""
 
 
 def server_timestamp():
     server_time = get_server_time()
-    parsed_t = parse(server_time)
-    timestamp = parsed_t.timestamp()
-    return timestamp
+    return int(server_time) / 1000
 
 
 def create_sign(timestamp: str, key_secret: str):
