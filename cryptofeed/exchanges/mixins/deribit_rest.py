5ae2b45c8c9f4a4b917acb15cb922bccac316725
Bryant Moscon <bmoscon@gmail.com>
2021-08-08 19:30:46 -0400

    Fix deribit

--- cryptofeed/exchanges/mixins/deribit_rest.py
+++ cryptofeed/exchanges/mixins/deribit_rest.py
@@ -4,12 +4,10 @@ Copyright (C) 2017-2021  Bryant Moscon - bmoscon@gmail.com
 Please see the LICENSE file for the terms and conditions
 associated with this software.
 '''
+import asyncio
 from decimal import Decimal
-from time import sleep
-from datetime import datetime as dt
 import logging
 
-import requests
 from sortedcontainers import SortedDict as sd
 from yapic import json
 
@@ -25,51 +23,22 @@ class DeribitRestMixin(RestExchange):
     sandbox_api = 'https://test.deribit.com/api/v2/public/'
     rest_channels = (TRADES, L2_BOOK)
 
-    def trades_sync(self, symbol: str, start=None, end=None, retry_count=1, retry_wait=10):
+    async def trades(self, symbol: str, start=None, end=None, retry_count=1, retry_delay=10):
         symbol = self.std_symbol_to_exchange_symbol(symbol)
-        for data in self._get_trades(symbol, start, end, retry, retry_wait):
-            yield data
-
-    def _get_trades(self, instrument, start_date, end_date, retry, retry_wait):
-        start = None
-        end = None
-
-        if start_date:
-            if not end_date:
-                end_date = dt.now()
-            start = self._datetime_normalize(start_date)
-            end = self._datetime_normalize(end_date)
-
+        start, end = self._interval_normalize(start, end)
+        if start:
             start = int(start * 1000)
             end = int(end * 1000)
 
-        @request_retry(self.id, retry, retry_wait)
-        def helper(start, end):
-            if start and end:
-                return requests.get(f"{self.api}get_last_trades_by_instrument_and_time?&start_timestamp={start}&end_timestamp={end}&instrument_name={instrument}&include_old=true&count=1000")
-            else:
-                return requests.get(f"{self.api}get_last_trades_by_instrument?instrument_name={instrument}&include_old=true&count=1000")
-
         while True:
-            r = helper(start, end)
+            endpoint = f"{self.api}get_last_trades_by_instrument?instrument_name={symbol}&include_old=true&count=1000"
+            if start and end:
+                endpoint = f"{self.api}get_last_trades_by_instrument_and_time?&start_timestamp={start}&end_timestamp={end}&instrument_name={symbol}&include_old=true&count=1000"
 
-            if r.status_code == 429:
-                sleep(int(r.headers['Retry-After']))
-                continue
-            elif r.status_code == 500:
-                LOG.warning("%s: 500 for URL %s - %s", self.id, r.url, r.text)
-                sleep(retry_wait)
-                continue
-            elif r.status_code != 200:
-                self._handle_error(r)
-            else:
-                sleep(1 / self.request_limit)
+            data = await self.http_conn.read(endpoint, retry_count=retry_count, retry_delay=retry_delay)
+            data = json.loads(data, parse_float=Decimal)["result"]["trades"]
 
-            data = json.loads(r.text, parse_float=Decimal)["result"]["trades"]
-            if data == []:
-                LOG.warning("%s: No data for range %d - %d",
-                            self.id, start, end)
-            else:
+            if data:
                 if data[-1]["timestamp"] == start:
                     LOG.warning("%s: number of trades exceeds exchange time window, some data will not be retrieved for time %d", self.id, start)
                     start += 1
@@ -82,6 +51,7 @@ class DeribitRestMixin(RestExchange):
 
             if len(orig_data) < 1000 or not start or not end:
                 break
+            await asyncio.sleep(1 / self.request_limit)
 
     def _trade_normalization(self, trade: list) -> dict:
 
@@ -96,39 +66,14 @@ class DeribitRestMixin(RestExchange):
         }
         return ret
 
-    def l2_book_sync(self, symbol: str, retry=0, retry_delay=60):
-        return self._book(symbol, retry=retry, retry_wait=retry_wait)
-
-    def _book(self, symbol: str, retry=0, retry_delay=60):
-        ret = {}
+    async def l2_book(self, symbol: str, retry_count=0, retry_delay=60):
         symbol = self.std_symbol_to_exchange_symbol(symbol)
         ret = {BID: sd(), ASK: sd()}
 
-        @request_retry(self.id, retry, retry_wait)
-        def helper():
-            return requests.get(f"{self.api}get_order_book?depth=10000&instrument_name={symbol}")
-
-        while True:
-            r = helper()
-
-            if r.status_code == 429:
-                sleep(int(r.headers['Retry-After']))
-                continue
-            elif r.status_code == 500:
-                LOG.warning("%s: 500 for URL %s - %s", self.id, r.url, r.text)
-                sleep(retry_wait)
-                if retry == 0:
-                    break
-                continue
-            elif r.status_code != 200:
-                self._handle_error(r)
-
-            data = json.loads(r.text, parse_float=Decimal)
-            break
-
+        data = await self.http_conn.read(f"{self.api}get_order_book?depth=10000&instrument_name={symbol}", retry_count=retry_count, retry_delay=retry_delay)
+        data = json.loads(data, parse_float=Decimal)
         for side, key in ((BID, 'bids'), (ASK, 'asks')):
             for entry_bid in data["result"][key]:
                 price, amount = entry_bid
                 ret[side][price] = amount
-
         return ret
