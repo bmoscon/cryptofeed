0d1c913ff330db4b1099e9fa561bfdf0cf0efbb9
Tristan M <81636151+tristan-murfitt-elw@users.noreply.github.com>
2021-10-09 19:17:58 +0100

    binance bug experiment (#606)
    * binance bug experiment
    
    * Add a fix for the order book issue
    
    * Bump order_book version to 0.3.2
    
    * Don't hard-code max_depth to a low value
    
    * Fix deltas on refreshed snapshots for async HTTP
    
    * Set max depth to oder books
    
    * Change book refresh logic to use time instead of number of messages
    
    * Remove redundant timestamp
    
    * Add random variation to snapshot refresh time and always fetch minimum of 100 orders when fetching a book snapshot
    
    * Update changelog
    
    * Remove debug log
    
    * Add missing fields to book callback, and add a new case for check update id
    
    * Add extra parameters for snapshot refresh time and fix max_depth
    
    * Move changelog changes to 2.0.2
    
    Co-authored-by: Ville Kuosmanen <ville.kuosmanen@elwood.io>

--- cryptofeed/exchanges/binance.py
+++ cryptofeed/exchanges/binance.py
@@ -5,6 +5,7 @@ Please see the LICENSE file for the terms and conditions
 associated with this software.
 '''
 import logging
+import random
 from asyncio import create_task, sleep
 from collections import defaultdict, deque
 from decimal import Decimal
@@ -22,6 +23,7 @@ from cryptofeed.symbols import Symbol
 from cryptofeed.exchanges.mixins.binance_rest import BinanceRestMixin
 from cryptofeed.types import Trade, Ticker, Candle, Liquidation, Funding, OrderBook, OrderInfo
 
+REFRESH_SNAPSHOT_MIN_INTERVAL_SECONDS = 60
 
 LOG = logging.getLogger('feedhandler')
 
@@ -43,6 +45,8 @@ class Binance(Feed, BinanceRestMixin):
         ORDER_INFO: ORDER_INFO
     }
     request_limit = 20
+    safe_snapshot_refresh_frequency = True  # Override this to `False` to refresh snapshots more frequently.
+                                            # Defaults to `True` to avoid hitting rate limits
 
     @classmethod
     def timestamp_normalize(cls, ts: float) -> float:
@@ -72,7 +76,34 @@ class Binance(Feed, BinanceRestMixin):
             info['instrument_type'][s.normalized] = stype
         return ret, info
 
-    def __init__(self, candle_closed_only=False, depth_interval='100ms', concurrent_http=False, **kwargs):
+    # Algorithm to decide how often new snapshots should be refreshed. Increases (non-linearly) as max_depth increases.
+    # Randomise the exact next refresh time so it's anywhere between 0.50x to 1.50x the actual time
+    # Example times:
+    #   max_depth <= 100: 1 minute
+    #   max_depth = 1000: 4 mins 45 seconds
+    #   max_depth = 5000: 15 mins 52 seconds
+    # To increase refresh frequency, set `safe_snapshot_refresh_frequency=False`
+    # It divides times to a fifth, allowing much more frequent refreshes but risks hitting rate limits
+    def _next_snapshot_refresh_interval(self) -> float:
+        max_depth = self.max_depth if self.max_depth else self.valid_depths[-1]
+        # Default: 1 minute
+        refresh_interval = REFRESH_SNAPSHOT_MIN_INTERVAL_SECONDS
+        if max_depth > 1000:
+            # Depth > 1000: 6 records per second
+            participating_depth = max_depth - 1000
+            refresh_interval += participating_depth / 6
+            max_depth -= participating_depth
+        if max_depth > 100:
+            # Depth > 100: 4 records per second
+            participating_depth = max_depth - 100
+            refresh_interval += participating_depth / 4
+            max_depth -= participating_depth
+        if not self.safe_snapshot_refresh_frequency:
+            refresh_interval = refresh_interval / 5
+        return refresh_interval * random.uniform(0.50, 1.50)
+
+
+    def __init__(self, candle_closed_only=False, depth_interval='100ms', concurrent_http=False, refresh_snapshot=False, **kwargs):
         """
         candle_closed_only: bool
             return only closed candles, i.e. no updates in between intervals.
@@ -80,6 +111,10 @@ class Binance(Feed, BinanceRestMixin):
             time between l2_book/delta updates {'100ms', '1000ms'} (different from BINANCE_FUTURES & BINANCE_DELIVERY)
         concurrent_http: bool
             http requests will be made concurrently, if False requests will be made one at a time (affects L2_BOOK, OPEN_INTEREST).
+        refresh_snapshot: bool
+            Automatically fetch new order book snapshots at regular intervals. Helps the order book from going stale, particularly at periods of large market price movements.
+            Turned off by default due to a risk of breaching rate limits when ubscribing to many symbols.
+            TODO turn it on by default when we can gracefully handle 429 errors
         """
         if depth_interval is not None and depth_interval not in self.valid_depth_intervals:
             raise ValueError(f"Depth interval must be one of {self.valid_depth_intervals}")
@@ -91,6 +126,10 @@ class Binance(Feed, BinanceRestMixin):
         self.depth_interval = depth_interval
         self.address = self._address()
         self.concurrent_http = concurrent_http
+        self.refresh_snapshot = refresh_snapshot
+        if self.refresh_snapshot:
+            self._next_snapshot_time: Dict[str, float] = {}
+            self._fetch_snapshop_buffer: Dict[str, deque[Tuple[dict, str, float]]] = {}
         self.token = None
 
         self._open_interest_cache = {}
@@ -266,6 +305,9 @@ class Binance(Feed, BinanceRestMixin):
         """
         if self._l2_book[std_pair].delta is None and msg['u'] <= self.last_update_id[std_pair]:
             return True
+        elif msg['U'] <= self.last_update_id[std_pair] and msg['u'] <= self.last_update_id[std_pair]:
+            # Old message, can ignore it
+            return True
         elif msg['U'] <= self.last_update_id[std_pair] + 1 <= msg['u']:
             self.last_update_id[std_pair] = msg['u']
             return False
@@ -277,8 +319,11 @@ class Binance(Feed, BinanceRestMixin):
             LOG.warning("%s: Missing book update detected, resetting book", self.id)
             return True
 
-    async def _snapshot(self, pair: str) -> None:
+    async def _fetch_snapshot(self, pair: str) -> None:
         max_depth = self.max_depth if self.max_depth else self.valid_depths[-1]
+        if max_depth < 100:
+            # Always fetch minimum of 100 depth snapshot
+            max_depth = 100
         if max_depth not in self.valid_depths:
             for d in self.valid_depths:
                 if d > max_depth:
@@ -289,12 +334,117 @@ class Binance(Feed, BinanceRestMixin):
         resp = await self.http_conn.read(url)
         await sleep(1 / self.request_limit)
         resp = json.loads(resp, parse_float=Decimal)
+        return resp
+
+    async def _reset_snapshot(self, pair: str) -> None:
+        resp = await self._fetch_snapshot(pair)
+        receipt_timestamp = time.time()
 
         std_pair = self.exchange_symbol_to_std_symbol(pair)
         self.last_update_id[std_pair] = resp['lastUpdateId']
         self._l2_book[std_pair] = OrderBook(self.id, std_pair, max_depth=self.max_depth, bids={Decimal(u[0]): Decimal(u[1]) for u in resp['bids']}, asks={Decimal(u[0]): Decimal(u[1]) for u in resp['asks']})
+        self._l2_book[std_pair].sequence_number = resp['lastUpdateId']
+        self._l2_book[std_pair].raw = resp
+        
+        if self.refresh_snapshot:
+            # Delete buffer and reset time since snapshot update
+            self._next_snapshot_time[std_pair] = receipt_timestamp + self._next_snapshot_refresh_interval()
+            self._fetch_snapshop_buffer.pop(std_pair, None)
         ts = self.timestamp_normalize(resp['E']) if 'E' in resp else None
-        await self.book_callback(L2_BOOK, self._l2_book[std_pair], time.time(), raw=resp, timestamp=ts, sequence_number=resp['lastUpdateId'])
+        await self.book_callback(L2_BOOK, self._l2_book[std_pair], receipt_timestamp, raw=resp, timestamp=ts, sequence_number=resp['lastUpdateId'])
+
+    async def _refresh_snapshot(self, pair: str) -> None:
+        resp = await self._fetch_snapshot(pair)
+        receipt_timestamp = time.time()
+
+        std_pair = self.exchange_symbol_to_std_symbol(pair)
+        self.last_update_id[std_pair] = resp['lastUpdateId']
+
+        delta = {BID: [], ASK: []}
+        for key, side in [('bids', BID) , ('asks', ASK)]:
+            for i, (price, amount) in enumerate(resp[key]):
+                price = Decimal(price)
+                amount = Decimal(amount)
+                if price in self._l2_book[std_pair].book[side]:
+                    if amount != self._l2_book[std_pair].book[side][price]:
+                        # Since we haven't looked at all the updates, changes in price are allowed here and will be published as deltas
+                        self._l2_book[std_pair].book[side][price] = amount
+                        delta[side].append((price, amount))
+                else:
+                    # Price not in order book: add it there
+                    self._l2_book[std_pair].book[side][price] = amount
+                    delta[side].append((price, amount))
+        self._l2_book[std_pair].sequence_number = resp['lastUpdateId']
+        
+        if self.refresh_snapshot:
+            # Delete buffer and reset time since snapshot update
+            self._next_snapshot_time[std_pair] = receipt_timestamp + self._next_snapshot_refresh_interval()
+            self._fetch_snapshop_buffer.pop(std_pair, None)
+
+        await self.book_callback(L2_BOOK, self._l2_book[std_pair], receipt_timestamp, raw=resp, delta=delta, sequence_number=self.last_update_id[std_pair])
+
+    async def _refresh_snapshot_with_buffer(self, pair: str) -> None:
+        resp = await self._fetch_snapshot(pair)
+        receipt_timestamp = time.time()
+
+        std_pair = self.exchange_symbol_to_std_symbol(pair)
+        if std_pair in self._book_buffer or std_pair not in self._fetch_snapshop_buffer:
+            # already fetching / fetched a snapshot, so ignore this message
+            self._fetch_snapshop_buffer.pop(std_pair, None)
+            return
+
+        while resp['lastUpdateId'] > self.last_update_id[std_pair]:
+            # Snapshot came before we received the corresponding WS update. Wait until we receive that.
+            if self.depth_interval == '100ms':
+                await sleep(0.1)
+            elif self.depth_interval == '1000ms':
+                await sleep(0.5)
+
+        # Construct a temporary book from snapshot
+        temp_book = OrderBook(self.id, std_pair, max_depth=self.max_depth, bids={Decimal(u[0]): Decimal(u[1]) for u in resp['bids']}, asks={Decimal(u[0]): Decimal(u[1]) for u in resp['asks']})
+
+        # Apply buffered messages to temporary book
+        while len(self._fetch_snapshop_buffer[std_pair]) > 0:
+            msg = self._fetch_snapshop_buffer[std_pair].popleft()
+            self._apply_msg_to_book(temp_book.book, msg)
+        
+        # Apply temporary book to local l2 book and resolve deltas
+        delta = {BID: [], ASK: []}
+        for side in [BID, ASK]:
+            for i, price in enumerate(temp_book.book[side]):
+                amount = temp_book.book[side][price]
+                if price in self._l2_book[std_pair].book[side]:
+                    if amount != self._l2_book[std_pair].book[side][price]:
+                        # This should never happen
+                        self._reset()
+                        LOG.error("%s: Book amount not equal to snapshot amount after fetching a refresh snapshot, resetting book", self.id)
+                        return
+                else:
+                    # Price not in order book: add it there
+                    self._l2_book[std_pair].book[side][price] = amount
+                    delta[side].append((price, amount))
+        
+        if self.refresh_snapshot:
+            # Delete buffer and reset time since snapshot update
+            self._next_snapshot_time[std_pair] = receipt_timestamp + self._next_snapshot_refresh_interval()
+            self._fetch_snapshop_buffer.pop(std_pair, None)
+        
+        await self.book_callback(L2_BOOK, self._l2_book[std_pair], receipt_timestamp, raw=resp, delta=delta, sequence_number=self.last_update_id[std_pair])
+
+    def _apply_msg_to_book(self, book: OrderBook, msg: dict) -> dict:
+        delta = {BID: [], ASK: []}
+        for s, side in (('b', BID), ('a', ASK)):
+            for i, update in enumerate(msg[s]):
+                price = Decimal(update[0])
+                amount = Decimal(update[1])
+                if amount == 0:
+                    if price in book[side]:
+                        del book[side][price]
+                        delta[side].append((price, amount))
+                else:
+                    book[side][price] = amount
+                    delta[side].append((price, amount))
+        return delta
 
     async def _handle_book_msg(self, msg: dict, pair: str, timestamp: float):
         """
@@ -305,20 +455,7 @@ class Binance(Feed, BinanceRestMixin):
         if skip_update:
             return
 
-        delta = {BID: [], ASK: []}
-        for s, side in (('b', BID), ('a', ASK)):
-            for update in msg[s]:
-                price = Decimal(update[0])
-                amount = Decimal(update[1])
-
-                if amount == 0:
-                    if price in self._l2_book[std_pair].book[side]:
-                        del self._l2_book[std_pair].book[side][price]
-                        delta[side].append((price, amount))
-                else:
-                    self._l2_book[std_pair].book[side][price] = amount
-                    delta[side].append((price, amount))
-
+        delta = self._apply_msg_to_book(self._l2_book[std_pair].book, msg)
         await self.book_callback(L2_BOOK, self._l2_book[std_pair], timestamp, timestamp=self.timestamp_normalize(msg['E']), raw=msg, delta=delta, sequence_number=self.last_update_id[std_pair])
 
     async def _book(self, msg: dict, pair: str, timestamp: float) -> None:
@@ -344,17 +481,33 @@ class Binance(Feed, BinanceRestMixin):
         }
         """
         std_pair = self.exchange_symbol_to_std_symbol(pair)
-        if std_pair in self._l2_book:
-            return await self._handle_book_msg(msg, pair, timestamp)
+        is_expired_snapshot = self.refresh_snapshot and std_pair in self._next_snapshot_time and time.time() > self._next_snapshot_time[std_pair]
 
         if not self.concurrent_http:
             # handle snapshot (a)synchronously
-            await self._snapshot(pair)
-            return await self._handle_book_msg(msg, pair, timestamp)
+            if std_pair not in self._l2_book:
+                await self._reset_snapshot(pair)
+            elif is_expired_snapshot:
+                # Refresh snapshot to make sure our data is still up to date
+                await self._refresh_snapshot(pair)
+            return await self._handle_book_msg(msg, pair, timestamp) 
 
         if std_pair in self._book_buffer:
             # snapshot is currently being fetched. std_pair will exist in self._l2_book after snapshot, but we need to continue buffering until all previous buffered messages have been processed.
             return self._book_buffer[std_pair].append((msg, pair, timestamp))
+
+        if is_expired_snapshot:
+            # Refresh snapshot to make sure our data is still up to date
+            if std_pair not in self._fetch_snapshop_buffer:
+                # Create a job to fetch a snapshot in the background
+                self._fetch_snapshop_buffer[std_pair] = deque()
+                create_task(self._refresh_snapshot_with_buffer(pair))
+            self._fetch_snapshop_buffer[std_pair].append(msg)
+            return await self._handle_book_msg(msg, pair, timestamp)
+
+        elif std_pair in self._l2_book:
+            # snapshot exists
+            return await self._handle_book_msg(msg, pair, timestamp)
 
         # concurrent http enabled and no buffer exists, so
         # initiate buffer & get snapshot
@@ -362,7 +515,7 @@ class Binance(Feed, BinanceRestMixin):
         self._book_buffer[std_pair].append((msg, pair, timestamp))
 
         async def _concurrent_snapshot():
-            await self._snapshot(pair)
+            await self._reset_snapshot(pair)
             while len(self._book_buffer[std_pair]) > 0:
                 book_args = self._book_buffer[std_pair].popleft()
                 await self._handle_book_msg(*book_args)
