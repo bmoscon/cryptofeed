7dfc8a59fb8a84065a69ceee12e501f260372cb7
Bryant Moscon <bmoscon@gmail.com>
2021-11-14 13:21:14 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -46,7 +46,7 @@ extension = Extension("cryptofeed.types", ["cryptofeed/types.pyx"],
 setup(
     name="cryptofeed",
     ext_modules=cythonize([extension], language_level=3, force=True),
-    version="2.1.0",
+    version="2.1.1",
     author="Bryant Moscon",
     author_email="bmoscon@gmail.com",
     description="Cryptocurrency Exchange Websocket Data Feed Handler",
