2d053b048cf2a799f7a2888a542402e5ba978f2b
DavideC <72390013+DavideColdebella@users.noreply.github.com>
2021-12-15 21:43:36 +0100

    Added TICKER import

--- cryptofeed/exchanges/huobi.py
+++ cryptofeed/exchanges/huobi.py
@@ -14,7 +14,7 @@ from decimal import Decimal
 from yapic import json
 
 from cryptofeed.connection import AsyncConnection
-from cryptofeed.defines import BUY, CANDLES, HUOBI, L2_BOOK, SELL, TRADES
+from cryptofeed.defines import BUY, CANDLES, HUOBI, L2_BOOK, SELL, TRADES, TICKER
 from cryptofeed.feed import Feed
 from cryptofeed.types import OrderBook, Trade, Candle
 
