d9e49f4c88fc18e3b246c0c69ee252a3f47d2f5a
ab <ablochs@gmail.com>
2021-12-29 10:05:19 +0100

    Simplify test

--- tests/unit/test_funding_type.py
+++ tests/unit/test_funding_type.py
@@ -6,49 +6,23 @@ from decimal import Decimal
 
 
 def test_funding_to_dict():
-    # data and data2 is example funding data from FTX
     data = {
-        "success": True,
-        "result": [
-            {
-                "future": "MER-PERP",
-                "rate": Decimal("-0.000001"),
-                "time": datetime.datetime(
-                    2021, 12, 26, 20, 0, tzinfo=datetime.timezone.utc
-                ),
-            },
-            {
-                "future": "MER-PERP",
-                "rate": Decimal("0.000015"),
-                "time": datetime.datetime(
-                    2021, 12, 26, 19, 0, tzinfo=datetime.timezone.utc
-                ),
-            },
-        ],
+        "exchange": "FTX",
+        "symbol": "BTC-USD-PERP",
+        "mark_price": Decimal("50000"),
+        "rate": Decimal("0.0002"),
+        "next_funding_time": Exchange.timestamp_normalize(
+            datetime.datetime(2021, 12, 26, 21, 0, tzinfo=datetime.timezone.utc)
+        ),
+        "predicted_rate": Decimal("0.0"),
+        "timestamp": Exchange.timestamp_normalize(
+            datetime.datetime(2021, 12, 26, 20, 0, tzinfo=datetime.timezone.utc)
+        ),
     }
-
-    data2 = {
-        "success": True,
-        "result": {
-            "volume": Decimal("3391482.0"),
-            "nextFundingRate": Decimal("0.0"),
-            "nextFundingTime": datetime.datetime(
-                2021, 12, 26, 21, 0, tzinfo=datetime.timezone.utc
-            ),
-            "openInterest": Decimal("1164922.0"),
-        },
-    }
-    data["predicted_rate"] = Decimal(data2["result"]["nextFundingRate"])
 
     f = Funding(
-        "FTX",
-        "MER-PERP",
-        None,
-        data["result"][0]["rate"],
-        Exchange.timestamp_normalize(data2["result"]["nextFundingTime"]),
-        Exchange.timestamp_normalize(data["result"][0]["time"]),
-        predicted_rate=data["predicted_rate"],
-        raw=[data, data2],
+        **data,
+        raw=data,
     )
     f_dict = f.to_dict(numeric_type=float, none_to=None)
 
