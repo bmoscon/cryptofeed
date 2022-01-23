14e95bcec3f3df3fdee06f39d9248c3a0dc9e8a3
Bryant Moscon <bmoscon@gmail.com>
2022-01-23 11:22:21 -0500

    Fix candles for influxDB

--- cryptofeed/backends/influxdb.py
+++ cryptofeed/backends/influxdb.py
@@ -124,3 +124,10 @@ class LiquidationsInflux(InfluxCallback, BackendCallback):
 
 class CandlesInflux(InfluxCallback, BackendCallback):
     default_key = 'candles'
+
+    async def write(self, data):
+        timestamp = data["timestamp"]
+        timestamp_str = f',timestamp={timestamp}' if timestamp is not None else ''
+        trades = f',trades={data["trades"]},' if data['trades'] else ','
+        update = f'{self.key}-{data["exchange"]},symbol={data["symbol"]},interval={data["interval"]} start={data["start"]},stop={data["stop"]}{trades}open={data["open"]},close={data["close"]},high={data["high"]},low={data["low"]},volume={data["volume"]}{timestamp_str},receipt_timestamp={data["receipt_timestamp"]} {int(data["receipt_timestamp"] * 1000000)}'
+        await self.queue.put({'data': update, 'headers': self.headers})
--- examples/demo_influxdb.py
+++ examples/demo_influxdb.py
@@ -5,15 +5,16 @@ Please see the LICENSE file for the terms and conditions
 associated with this software.
 '''
 from cryptofeed import FeedHandler
-from cryptofeed.backends.influxdb import BookInflux, FundingInflux, TickerInflux, TradeInflux
-from cryptofeed.defines import FUNDING, L2_BOOK, TICKER, TRADES
+from cryptofeed.backends.influxdb import BookInflux, CandlesInflux, FundingInflux, TickerInflux, TradeInflux
+from cryptofeed.defines import CANDLES, FUNDING, L2_BOOK, TICKER, TRADES
 from cryptofeed.exchanges import Bitmex, Coinbase
+from cryptofeed.exchanges.binance import Binance
 
 
 INFLUX_ADDR = 'http://localhost:8086'
-ORG = 'crypto'
-BUCKET = 'crypto'
-TOKEN = 'XXXXXXX'
+ORG = 'cryptofeed'
+BUCKET = 'cryptofeed'
+TOKEN = 'XXXXXXXXXX'
 
 
 def main():
@@ -23,7 +24,7 @@ def main():
     f.add_feed(Coinbase(channels=[TRADES], symbols=['BTC-USD'], callbacks={TRADES: TradeInflux(INFLUX_ADDR, ORG, BUCKET, TOKEN)}))
     f.add_feed(Coinbase(channels=[L2_BOOK], symbols=['BTC-USD'], callbacks={L2_BOOK: BookInflux(INFLUX_ADDR, ORG, BUCKET, TOKEN)}))
     f.add_feed(Coinbase(channels=[TICKER], symbols=['BTC-USD'], callbacks={TICKER: TickerInflux(INFLUX_ADDR, ORG, BUCKET, TOKEN)}))
-
+    f.add_feed(Binance(candle_closed_only=False, channels=[CANDLES], symbols=['BTC-USDT'], callbacks={CANDLES: CandlesInflux(INFLUX_ADDR, ORG, BUCKET, TOKEN)}))
     f.run()
 
 
