6cf185de641cd63b180f2a34cf52c0773e5961a3
Bryant Moscon <bmoscon@gmail.com>
2022-04-14 19:59:54 -0400

    Fix questdb

--- cryptofeed/backends/quest.py
+++ cryptofeed/backends/quest.py
@@ -24,12 +24,8 @@ class QuestCallback(SocketCallback):
     async def writer(self):
         while True:
             await self.connect()
-            count = self.queue.qsize()
-            if count == 0:
-                count = 1
-
-            async with self.read_many_queue(count) as update:
-                update = "\n".join(update) + "\n"
+            async with self.read_queue() as updates:
+                update = "\n".join(updates) + "\n"
                 self.conn.write(update.encode())
 
     async def write(self, data):
