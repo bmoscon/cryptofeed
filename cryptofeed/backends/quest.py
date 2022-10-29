7e51606d8bd236d7636f3e1f274a5a1c68fa5034
merowinger92 <merowinger92@users.noreply.github.com>
2022-10-29 20:25:16 +0300

    fix flake8

--- cryptofeed/backends/quest.py
+++ cryptofeed/backends/quest.py
@@ -30,7 +30,7 @@ class QuestCallback(SocketCallback):
 
     async def write(self, data):
         d = self.format(data)
-        d = f",{d}" if len(d) > 0 else '' 
+        d = f",{d}" if len(d) > 0 else ''
         timestamp = data["timestamp"]
         received_timestamp_int = int(data["receipt_timestamp"] * 1_000_000)
         timestamp_int = int(timestamp * 1_000_000_000) if timestamp is not None else received_timestamp_int * 1000
@@ -74,7 +74,7 @@ class BookQuest(QuestCallback):
 
     async def __call__(self, book, receipt_timestamp: float):
         vals = ','.join([f"bid_{i}_price={book.book.bids.index(i)[0]},bid_{i}_size={book.book.bids.index(i)[1]}" for i in range(self.depth)] + [f"ask{i}_price={book.book.asks.index(i)[0]},ask_{i}_size={book.book.asks.index(i)[1]}" for i in range(self.depth)])
-        vals = f",{vals}" if len(vals) > 0 else '' 
+        vals = f",{vals}" if len(vals) > 0 else ''
         timestamp = book.timestamp
         receipt_timestamp_int = int(receipt_timestamp * 1_000_000)
         timestamp_int = int(timestamp * 1_000_000_000) if timestamp is not None else receipt_timestamp_int * 1000
