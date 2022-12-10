67aa48622e7bb39c371e53b980b59676dc0975f5
Pete Hope <petehope@gmail.com>
2022-12-10 08:48:45 +0000

    :sparkles: Access to all AIOKafka config options
    Uses the kwarg `producer_config` to pass a dictionary containing
    any available configuration options to AIOKafkaProducer.
    Provides sensible defaults if none provided. (Same as previous defaults)

--- cryptofeed/backends/kafka.py
+++ cryptofeed/backends/kafka.py
@@ -6,7 +6,7 @@ associated with this software.
 '''
 from collections import defaultdict
 import asyncio
-from typing import Optional
+from typing import Optional, DefaultDict, List, Callable
 
 from aiokafka import AIOKafkaProducer
 from yapic import json
@@ -15,31 +15,33 @@ from cryptofeed.backends.backend import BackendBookCallback, BackendCallback
 
 
 class KafkaCallback:
-    def __init__(self, bootstrap='127.0.0.1', port=9092, key=None, numeric_type=float, none_to=None, acks=0, client_id='cryptofeed', **kwargs):
+    def __init__(self, producer_config: DefaultDict[str, str | List | int | Callable] | None = None, key=None, numeric_type=float, none_to=None, **kwargs):
         """
-        bootstrap: str, list
-            if a list, should be a list of strings in the format: ip/host:port, i.e.
-                192.1.1.1:9092
-                192.1.1.2:9092
-                etc
-            if a string, should be ip/port only
+        producer_config: dict
+            A dictionary of configuration settings for AIOKafkaProducer.
+            A 'value_serializer' option allows use of various alternative schemas such as Protobuf, Avro, etc. 
+            The default serialization is JSON Bytes
+            A full list of configuration parameters can be found at https://aiokafka.readthedocs.io/en/stable/api.html#aiokafka.AIOKafkaProducer  
+            Example:
+            {'bootstrap_servers': '127.0.0.1:9092',
+            'client_id': 'cryptofeed',
+            'acks': 1}
+            (the event loop is provided by Cryptofeed)
         """
-        self.bootstrap = bootstrap
-        self.port = port
+        self.producer_config = producer_config or {
+            'bootstrap_servers': '127.0.0.1:9092',
+            'client_id': 'cryptofeed',
+            'acks': 0
+        }
         self.producer = None
         self.key = key if key else self.default_key
         self.numeric_type = numeric_type
         self.none_to = none_to
-        self.acks = acks
-        self.client_id = client_id
 
     async def __connect(self):
         if not self.producer:
             loop = asyncio.get_event_loop()
-            self.producer = AIOKafkaProducer(acks=self.acks,
-                                             loop=loop,
-                                             bootstrap_servers=f'{self.bootstrap}:{self.port}' if isinstance(self.bootstrap, str) else self.bootstrap,
-                                             client_id=self.client_id)
+            self.producer = AIOKafkaProducer(**self.producer_config, loop=loop)
             await self.producer.start()
 
     def topic(self, data: dict) -> str:
