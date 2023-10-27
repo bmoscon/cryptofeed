b0dbcf97e744e921320522a0c936ef5024d1b76d
igorniebylski <igor.niebylski@gmail.com>
2023-10-27 16:03:23 +0200

    updated quasar demo

--- examples/demo_quasar.py
+++ examples/demo_quasar.py
@@ -4,12 +4,11 @@ from cryptofeed.backends.quasar import *
 
 
 def main():
-    f = FeedHandler()    
-    pairs = BinanceFutures.symbols[:1]
-    f.add_feed(BinanceFutures(symbols=pairs, channels=[FUNDING], callbacks={FUNDING: FundingQuasar()}))
-    f.add_feed(Binance(candle_closed_only=False, channels=[CANDLES], symbols=['BTC-USDT'], callbacks={CANDLES: CandlesQuasar()}))
-    f.add_feed(Coinbase(symbols=['BTC-USD'], channels=[TRADES], callbacks={TRADES: TradeQuasar()}))
+    f = FeedHandler()
+    
+    f.add_feed(Binance(symbols=['BTC-USDT'], channels=[TRADES], callbacks={TRADES: TradeQuasar()}))
     f.add_feed(Coinbase(channels=[TICKER], symbols=['BTC-USD'], callbacks={TICKER: TickerQuasar()}))
+    f.add_feed(Binance(candle_closed_only=False, channels=[CANDLES], symbols=['BTC-USDT'], callbacks={CANDLES: CandlesQuasar()}))
     
     f.run()
 
