5d920506cbd2376ffcb4bf90d7758888c22775e1
igorniebylski <igor.niebylski@gmail.com>
2023-10-30 09:27:22 +0100

    added shard_size to QuasarCallback, added table creation, added comments explaining _set_table_name method

--- cryptofeed/backends/quasar.py
+++ cryptofeed/backends/quasar.py
@@ -4,14 +4,14 @@ import quasardb.pandas as qdbpd
 from cryptofeed.backends.backend import (BackendBookCallback,BackendCallback,BackendQueue)
 
 class QuasarCallback(BackendQueue):
-    def __init__(self, host="127.0.0.1", port=2836, username:str="", private_key:str="", public_key:str="", table:str=None, none_to=None):
+    def __init__(self, host="127.0.0.1", port=2836, username:str="", private_key:str="", public_key:str="", none_to=None, shard_size:timedelta = timedelta(minutes=15)):
         self.numeric_type = float
-        self.table_prefix = table if table else self.default_table
-        self.table = self.table_prefix
+        self.table = ""
         self.running = True
         self.url = f"qdb://{host}:{port}"
         self.none_to = none_to
         self.columns=[]
+        self.shard_size = shard_size
         
         with quasardb.Cluster(self.url, user_name=username, user_private_key=private_key, cluster_public_key=public_key) as conn:
             self.conn = conn
@@ -24,19 +24,27 @@ class QuasarCallback(BackendQueue):
         return data
 
     def _set_table_name(self, data: dict):
-        self.table = f"{data['exchange'].capitalize()}/{self.table_prefix}/{data['symbol']}"
+        # setting table name 
+        # {channel}/{exchange}/{symbol_1-symbol_2}
+        # eg. ticker/coinbase/btc-usd
+        self.table = f"{self.table_prefix.lower()}/{data['exchange'].lower()}/{data['symbol'].lower()}"
                 
     async def write(self, data: dict):
         self._set_table_name(data)
         data = self.format(data)
         df = qdbpd.DataFrame(data, columns=self.columns, index=[data['timestamp']])
-        qdbpd.write_dataframe(df, self.conn, self.conn.table(self.table), fast=True, _async=True)
+        # write to table, if table doesnt exist it will be created with specified shard_size value
+        qdbpd.write_dataframe(df, self.conn, self.conn.table(self.table), fast=True, _async=True, create=True, shard_size=self.shard_size)
 
 class TickerQuasar(QuasarCallback, BackendCallback):
-    default_table = "Ticker"
+    table_prefix = "ticker"
                 
 class TradeQuasar(QuasarCallback, BackendCallback):
-    default_table = "Trades"
+    table_prefix = "trades"
     
     def _set_table_name(self, data: dict):
-        self.table = f"{data['exchange'].capitalize()}/{self.table_prefix}/{data['symbol']}/{data['side']}"
+        # setting table name depending on side
+        # trades/{exchange}/{symbol_1-symbol_2}/{side}
+        # eg. trade/coinbase/btc-usd/buy
+        super()._set_table_name(data)
+        self.table = f"{self.table}/{data['side'].lower()}"
