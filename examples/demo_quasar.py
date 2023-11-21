5dcd5c4aed3819008fe76b5588500cb98fde43fe
igorniebylski <igor.niebylski@gmail.com>
2023-11-21 13:25:44 +0100

    removed binance

--- examples/demo_quasar.py
+++ examples/demo_quasar.py
@@ -2,32 +2,28 @@ from cryptofeed import FeedHandler
 from cryptofeed.exchanges import *
 from cryptofeed.backends.quasar import *
 
+
 async def ticker_info(ticker, receipt_timestamp):
     print(f'Ticker: {ticker} at {receipt_timestamp}')
+
 
 async def trade_info(trade, receipt_timestamp):
     print(f'Trade: {trade} at {receipt_timestamp}')
 
-async def candle_info(candle, receipt_timestamp):
-    print(f'Candle {candle} at {receipt_timestamp}')
-
 
 def main():
     f = FeedHandler()
-    
+
     # print to console
-    f.add_feed(Binance(candle_closed_only=False, channels=[CANDLES], symbols=['BTC-USDT'], callbacks={CANDLES: candle_info}))
-    f.add_feed(Binance(symbols=['BTC-USDT'], channels=[TRADES], callbacks={TRADES: trade_info}))
     f.add_feed(Coinbase(channels=[TICKER], symbols=['BTC-USD'], callbacks={TICKER: ticker_info}))
     f.add_feed(Coinbase(symbols=['BTC-USD'], channels=[TRADES], callbacks={TRADES: trade_info}))
-    
+
     # save to database
-    f.add_feed(Binance(candle_closed_only=False, channels=[CANDLES], symbols=['BTC-USDT'], callbacks={CANDLES: CandlesQuasar()}))
-    f.add_feed(Binance(symbols=['BTC-USDT'], channels=[TRADES], callbacks={TRADES: TradeQuasar()}))
     f.add_feed(Coinbase(channels=[TICKER], symbols=['BTC-USD'], callbacks={TICKER: TickerQuasar()}))
     f.add_feed(Coinbase(symbols=['BTC-USD'], channels=[TRADES], callbacks={TRADES: TradeQuasar()}))
 
     f.run()
+
 
 if __name__ == '__main__':
     main()
