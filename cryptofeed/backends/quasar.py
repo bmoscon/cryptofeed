8ab1d713bae29207d838c65f09f2aec5c6070c6b
igorniebylski <igor.niebylski@gmail.com>
2023-11-21 13:25:04 +0100

    symbol column is now of symbol type, only one table per callback, create table query is created for each callback type

--- cryptofeed/backends/quasar.py
+++ cryptofeed/backends/quasar.py
@@ -11,10 +11,16 @@ class QuasarCallback(BackendQueue):
         self.running = True
         self.none_to = none_to
         self.columns = []
-        self.shard_size = shard_size
+        self.shard_size = self._get_str_timedelta(shard_size)
         url = f"qdb://{host}:{port}"
 
         pool.initialize(uri=url, user_name=username, user_private_key=private_key, cluster_public_key=public_key)
+        
+    def _get_str_timedelta(self, delta: timedelta):
+        # calculate the number of hours, minutes, and remaining seconds from timedelta, return it in correct format for query
+        hours, remainder = divmod(delta.total_seconds(), 3600)
+        minutes, seconds = divmod(remainder, 60)
+        return f"{int(hours)}hour {int(minutes)}min {int(seconds)}s"
 
     def format(self, data: dict):
         self.columns = list(data.keys())
@@ -28,29 +34,34 @@ class QuasarCallback(BackendQueue):
         # {channel}/{exchange}/{symbol_1-symbol_2}
         # eg. ticker/coinbase/btc-usd
         self.table = f"{self.table_prefix.lower()}/{data['exchange'].lower()}/{data['symbol'].lower()}"
+        
+    def _create_table(self, conn):
+        if not conn.table(self.table).exists():
+            conn.query(self.query)
 
     async def write(self, data: dict):
         self._set_table_name(data)
+        self._create_query()
         data = self.format(data)
         df = qdbpd.DataFrame(data, columns=self.columns, index=[data['timestamp']])
         # write to table, if table doesnt exist it will be created with specified shard_size value
         with pool.instance().connect() as conn:
-            qdbpd.write_dataframe(df, conn, conn.table(self.table), fast=True, _async=True, create=True, shard_size=self.shard_size)
+            self._create_table(conn)
+            qdbpd.write_dataframe(df, conn, conn.table(self.table), fast=True, _async=True)
 
 
 class TickerQuasar(QuasarCallback, BackendCallback):
     table_prefix = "ticker"
+    
+    def _create_query(self):
+        self.query = f'CREATE TABLE "{self.table}" (exchange STRING, symbol SYMBOL(currency), bid DOUBLE, ask DOUBLE, receipt_timestamp TIMESTAMP) SHARD_SIZE = {self.shard_size}'
 
 
 class TradeQuasar(QuasarCallback, BackendCallback):
     table_prefix = "trades"
-
-    def _set_table_name(self, data: dict):
-        # setting table name depending on side
-        # trades/{exchange}/{symbol_1-symbol_2}/{side}
-        # eg. trade/coinbase/btc-usd/buy
-        super()._set_table_name(data)
-        self.table = f"{self.table}/{data['side'].lower()}"
+    
+    def _create_query(self):
+        self.query = f'CREATE TABLE "{self.table}" (exchange STRING, symbol SYMBOL(currency), side STRING, amount DOUBLE, price DOUBLE, id STRING, type STRING, receipt_timestamp TIMESTAMP) SHARD_SIZE = {self.shard_size}'
 
 
 class CandlesQuasar(QuasarCallback, BackendCallback):
@@ -62,3 +73,6 @@ class CandlesQuasar(QuasarCallback, BackendCallback):
         data['stop'] = datetime.utcfromtimestamp(data['stop'])
         data['closed'] = int(data['closed'])
         return data
+    
+    def _create_query(self):
+        self.query = f'CREATE TABLE "{self.table}" (exchange STRING, symbol SYMBOL(currency), start TIMESTAMP, stop TIMESTAMP, interval STRING, trades STRING, open DOUBLE, close DOUBLE, high DOUBLE, low DOUBLE, volume DOUBLE, closed INT64) SHARD_SIZE = {self.shard_size}'
