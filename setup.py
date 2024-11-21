90ebe028be905d19fc4e3a1aaf9d7bddf489e0a8
Bryant Moscon <bmoscon@gmail.com>
2024-11-20 19:12:01 -0500

    Update reqs

--- setup.py
+++ setup.py
@@ -76,7 +76,7 @@ setup(
     tests_require=["pytest"],
     install_requires=[
         "requests>=2.18.4",
-        "websockets>=10.0",
+        "websockets>=14.1",
         "pyyaml",
         "aiohttp>=3.11.6",
         "aiofile>=2.0.0",
