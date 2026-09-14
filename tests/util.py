ba6ba83d690efe369ec8780403720b2145dccc1e
Bryant Moscon <bmoscon@gmail.com>
2026-09-14 18:45:20 -0400

    Playback tests run on smaller data set

--- tests/util.py
+++ tests/util.py
@@ -7,16 +7,27 @@ associated with this software.
 import json
 import os
 
+from cryptofeed.capture import Replayer, playback
 from cryptofeed.capture.recorder import metadata_path
+from cryptofeed.defines import INDEPENDENT_RESERVE
 
 
 ROOT = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-SAMPLE_DATA = os.path.join(ROOT, 'sample_data')
 CONFIG = os.path.join(ROOT, 'tests', 'config_test.yaml')
+DEFAULT_SAMPLE_DATA = os.path.join(ROOT, 'sample_data', 'small')
+
+# attributes to inject before replay
+feed_overrides = {
+    INDEPENDENT_RESERVE: {'request_limit': 1_000_000, 'SNAPSHOT_STALENESS_WAIT': 0},
+}
+
+
+def sample_data() -> str:
+    return os.environ.get('CRYPTOFEED_SAMPLE_DATA') or DEFAULT_SAMPLE_DATA
 
 
 def capture_path(exchange: str) -> str:
-    return os.path.join(SAMPLE_DATA, f'{exchange}.pcap.zst')
+    return os.path.join(sample_data(), f'{exchange}.pcap.zst')
 
 
 def read_metadata(exchange: str) -> dict:
@@ -26,3 +37,20 @@ def read_metadata(exchange: str) -> dict:
 
 def feed_entry(exchange: str) -> dict:
     return next(entry for entry in read_metadata(exchange)['feeds'] if entry['exchange'] == exchange)
+
+
+def expected_callbacks() -> dict:
+    with open(os.path.join(sample_data(), 'expected_callbacks.json')) as fp:
+        return json.load(fp)
+
+
+def replay(exchange: str):
+    path = capture_path(exchange)
+    feed = None
+
+    if exchange in feed_overrides:
+        feed = Replayer(path).build_feed(config=CONFIG)
+        for attr, value in feed_overrides[exchange].items():
+            setattr(feed, attr, value)
+
+    return playback(path, feed=feed, config=CONFIG, on_error='raise')
